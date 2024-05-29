4714800a82e125676ef212ebf984a876acb3551f
CHOIJIONE\choij <choijione02@gmail.com>
2024-05-29 13:55:28 +0900

    엔진 모듈 초기화 및 아무 동작도 수행하지 않는 게임 루프 추가

--- CoinDash3D/Src/Main.cpp
+++ CoinDash3D/Src/Main.cpp
@@ -5,11 +5,36 @@
 #include <crtdbg.h>
 #endif
 
+#include "CrashModule.h"
+#include "GameModule.h"
+#include "PlatformModule.h"
+#include "RenderModule.h"
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
+	PlatformModule::WindowConstructParams windowParam{ L"CoinDash3D", 100, 100, 800, 600, false, false };
+
+	CrashModule::Init();
+	PlatformModule::Init(windowParam);
+	RenderModule::Init(PlatformModule::GetWindowHandle());
+	GameModule::Init();
+
+	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
+
+	PlatformModule::RunLoop(
+		[&](float deltaSeconds) 
+		{
+			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+			RenderModule::EndFrame();
+		}
+	);
+
+	GameModule::Uninit();
+	PlatformModule::Uninit();
+	CrashModule::Uninit();
 	return 0;
 }
