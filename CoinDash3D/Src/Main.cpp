1c0c52cb0ddef9f60e34a840819c9da0bb2e567d
DESKTOP-CIQEUKA\choij <choijione02@gmail.com>
2024-06-02 18:37:28 +0900

    이전의 메인 함수 내의 코드 부분 복원

--- CoinDash3D/Src/Main.cpp
+++ CoinDash3D/Src/Main.cpp
@@ -37,12 +37,88 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	RenderModule::Init(PlatformModule::GetWindowHandle());
 	GameModule::Init();
 
+	GeometryRenderer3D* geometryRenderer = RenderModule::CreateResource<GeometryRenderer3D>();
+	DepthRenderer3D* depthRenderer = RenderModule::CreateResource<DepthRenderer3D>();
+	MeshRenderer3D* meshRenderer = RenderModule::CreateResource<MeshRenderer3D>();
+	ShadowMap* shadowMap = RenderModule::CreateResource<ShadowMap>(ShadowMap::ESize::Size_1024x1024);
+
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
-	
+
+	Floor* floor = GameModule::CreateEntity<Floor>();
+	Wall* wall = GameModule::CreateEntity<Wall>();
+	Character* character = GameModule::CreateEntity<Character>(wall);
+	Camera* camera = GameModule::CreateEntity<Camera>(character);
+	Coin* coin = GameModule::CreateEntity<Coin>(Vec3f(1.0f, 0.5f, 1.0f));
+	Light* light = GameModule::CreateEntity<Light>(character);
+
 	PlatformModule::RunLoop(
-		[&](float deltaSeconds) 
+		[&](float deltaSeconds)
 		{
+			character->Tick(deltaSeconds);
+			camera->Tick(deltaSeconds);
+			light->Tick(deltaSeconds);
+
+			depthRenderer->SetLightView(light->GetView());
+			depthRenderer->SetLightProjection(light->GetProjection());
+			geometryRenderer->SetView(camera->GetView());
+			geometryRenderer->SetProjection(camera->GetProjection());
+			meshRenderer->SetView(camera->GetView());
+			meshRenderer->SetProjection(camera->GetProjection());
+			meshRenderer->SetViewPosition(camera->GetEyePosition());
+			meshRenderer->SetShadowMap(shadowMap);
+			meshRenderer->SetLightView(light->GetView());
+			meshRenderer->SetLightProjection(light->GetProjection());
+			meshRenderer->SetLightDirection(light->GetDirection());
+			meshRenderer->SetLightColor(light->GetColor());
+
+			shadowMap->Bind();
+			{
+				shadowMap->Clear();
+				RenderModule::SetViewport(0, 0, shadowMap->GetSize(), shadowMap->GetSize());
+
+				std::vector<StaticMesh*>& staticMeshes = wall->GetMeshes();
+				std::vector<Transform>& transforms = wall->GetTransforms();
+
+				for (uint32_t index = 0; index < staticMeshes.size(); ++index)
+				{
+					depthRenderer->DrawStaticMesh3D(Transform::ToMat(transforms[index]), staticMeshes[index]);
+				}
+
+				depthRenderer->DrawStaticMesh3D(Transform::ToMat(floor->GetTransform()), floor->GetMesh());
+				depthRenderer->DrawStaticMesh3D(Transform::ToMat(coin->GetTransform()), coin->GetMesh());
+
+				std::vector<SkinnedMesh*>& skinnedMeshes = character->GetMeshes();
+				for (const auto& skinnedMesh : skinnedMeshes)
+				{
+					const std::vector<Mat4x4>& bindPose = skinnedMesh->GetPosePalette();
+					const std::vector<Mat4x4>& invBindPose = character->GetCrossFadeController().GetSkeleton().GetInvBindPose();
+					depthRenderer->DrawSkinnedMesh3D(Transform::ToMat(character->GetTransform()), skinnedMesh, bindPose, invBindPose);
+				}
+			}
+			shadowMap->Unbind();
+
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+			{
+				std::vector<StaticMesh*>& staticMeshes = wall->GetMeshes();
+				std::vector<Transform>& transforms = wall->GetTransforms();
+				ITexture2D* material = wall->GetMaterial();
+
+				for (uint32_t index = 0; index < staticMeshes.size(); ++index)
+				{
+					meshRenderer->DrawStaticMesh3D(Transform::ToMat(transforms[index]), staticMeshes[index], material);
+				}
+
+				meshRenderer->DrawStaticMesh3D(Transform::ToMat(floor->GetTransform()), floor->GetMesh(), floor->GetMaterial());
+				meshRenderer->DrawStaticMesh3D(Transform::ToMat(coin->GetTransform()), coin->GetMesh(), coin->GetMaterial());
+
+				std::vector<SkinnedMesh*>& skinnedMeshes = character->GetMeshes();
+				for (const auto& skinnedMesh : skinnedMeshes)
+				{
+					const std::vector<Mat4x4>& bindPose = skinnedMesh->GetPosePalette();
+					const std::vector<Mat4x4>& invBindPose = character->GetCrossFadeController().GetSkeleton().GetInvBindPose();
+					meshRenderer->DrawSkinnedMesh3D(Transform::ToMat(character->GetTransform()), skinnedMesh, bindPose, invBindPose, character->GetMaterial());
+				}
+			}
 			RenderModule::EndFrame();
 		}
 	);
