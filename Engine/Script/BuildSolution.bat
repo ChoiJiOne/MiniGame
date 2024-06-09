a09ad731ce5fc9817be2715dd804ae166134c8ff
choijione <choijione02@gmail.com>
2024-06-09 17:40:42 +0900

    빌드 스크립트의 코딩 컨벤션 수정

--- Engine/Script/BuildSolution.bat
+++ Engine/Script/BuildSolution.bat
@@ -1,53 +1,52 @@
-@echo off
+@ECHO OFF
 
-echo =====================
-echo Start Build Solution
-echo =====================
+ECHO =====================
+ECHO Start Build Solution
+ECHO =====================
 
 @REM CMake가 설치되었는지 확인합니다.
-@echo off
 where cmake > nul 2>&1
 if %ERRORLEVEL% EQU 0 (
-    echo CMake is installed on this system...
+    ECHO CMake is installed on this system...
 ) else (
-    echo CMake is not installed on this system...
+    ECHO CMake is not installed on this system...
     GOTO:EOF
 )
 
-set solutionPath=%~dp0..\..\Solution
+SET SOLUTION_PATH=%~dp0..\..\Solution
 
 @REM 프로젝트가 존재하지 않으면 프로젝트를 생성합니다.
-if not exist %solutionPath% (
-    mkdir %solutionPath%
+if not exist %SOLUTION_PATH% (
+    mkdir %SOLUTION_PATH%
 )
 
 @REM 빌드할 모드입니다.
-SET mode=%1
+SET MODE=%1
 
 @REM 빌드 모드의 유효성을 검사합니다.
-if "%mode%" == "Debug" (
-    echo Build "%mode%" mode...
-) else if "%mode%" == "Release" (
-    echo Build "%mode%" mode...
-) else if "%mode%" == "RelWithDebInfo" (
-    echo Build "%mode%" mode...
-) else if "%mode%" == "MinSizeRel" (
-    echo Build "%mode%" mode...
+if "%MODE%" == "Debug" (
+    ECHO Build "%MODE%" mode...
+) else if "%MODE%" == "Release" (
+    ECHO Build "%MODE%" mode...
+) else if "%MODE%" == "RelWithDebInfo" (
+    ECHO Build "%MODE%" mode...
+) else if "%MODE%" == "MinSizeRel" (
+    ECHO Build "%MODE%" mode...
 ) else (
-    echo "%mode%" is illegal mode...
+    ECHO "%MODE%" is illegal mode...
     GOTO:EOF
 )
 
-set currentPath=%~dp0
+SET CURRENT_PATH=%~dp0
 
-pushd "%currentPath%"
-pushd "%solutionPath%"
+PUSHD "%CURRENT_PATH%"
+PUSHD "%SOLUTION_PATH%"
 
 cmake --build . --config %mode% --clean-first --parallel  
 
-popd
-popd
+POPD
+POPD
 
-echo =====================
-echo Done Build Solution
-echo =====================
+ECHO =====================
+ECHO Done Build Solution
+ECHO =====================
