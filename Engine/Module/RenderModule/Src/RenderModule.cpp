61634e52f57a9cc8e46793d703a8a4d4011028a5
choijione <choijione02@gmail.com>
2024-06-13 08:22:02 +0900

    렌더 모듈 초기화 시 포인트 크기 모드는 기본적으로 활성화하도록 변경

--- Engine/Module/RenderModule/Src/RenderModule.cpp
+++ Engine/Module/RenderModule/Src/RenderModule.cpp
@@ -54,8 +54,8 @@ RenderModule::Errors RenderModule::Init(HWND windowHandle)
 		return Errors::ERR_IMGUI;
 	}
 
-	// 기본적으로 알파 블랜딩 모드는 활성화.
-	SetAlphaBlendMode(true);
+	SetAlphaBlendMode(true);// 기본적으로 알파 블랜딩 모드는 활성화.
+	SetPointSizeMode(true); // 기본적으로 포인트 크기 모드는 활성화.
 
 	bIsInit = true;
 	return Errors::OK;
