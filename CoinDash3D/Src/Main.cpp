9eee4f3bc19834c4be21b0c52ea0f43e50be74f2
choijione <choijione02@gmail.com>
2024-06-13 09:29:02 +0900

    3D 모델 로딩 및 렌더링을 수행하는 테스트 코드 추가

--- CoinDash3D/Src/Main.cpp
+++ CoinDash3D/Src/Main.cpp
@@ -22,6 +22,10 @@
 #include "Shader.h"
 #include "GLTFLoader.h"
 #include "CrossFadeController.h"
+#include "BaseColorMap.h"
+#include "TileColorMap.h"
+
+#include <imgui.h>
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -41,14 +45,42 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	GeometryRenderer2D* renderer2d = RenderModule::CreateResource<GeometryRenderer2D>();
 	GeometryRenderer3D* renderer3d = RenderModule::CreateResource<GeometryRenderer3D>();
 	TextRenderer* textRenderer = RenderModule::CreateResource<TextRenderer>();
-	TTFont* font = RenderModule::CreateResource<TTFont>("Resource/Font/SeoulNamsanEB.ttf", 0, 0x127, 24.0f);
-
+	TTFont* font = RenderModule::CreateResource<TTFont>("Resource/Font/SeoulNamsanEB.ttf", 0, 0x127, 32.0f);
+	BaseColorMap* baseColorMap = RenderModule::CreateResource<BaseColorMap>(BaseColorMap::ESize::Size_1024x1024, Vec4f(1.0f, 0.5f, 0.0f, 1.0f));
+	TileColorMap* tileColorMap = RenderModule::CreateResource<TileColorMap>(
+		TileColorMap::ESize::Size_1024x1024, 
+		TileColorMap::ESize::Size_16x16,
+		Vec4f(1.0f, 1.0f, 1.0f, 1.0f),
+		Vec4f(1.0f, 0.0f, 0.0f, 1.0f)
+	);
 	Camera* camera = GameModule::CreateEntity<Camera>();
 
-	RenderModule::SetPointSizeMode(true);
 	Mat4x4 screenOrtho = RenderModule::GetScreenOrtho();
 	renderer2d->SetOrtho(screenOrtho);
 	textRenderer->SetOrtho(screenOrtho);
+
+	cgltf_data* data = GLTFLoader::Load("Resource/Model/test11.glb");
+	std::vector<GLTFLoader::MeshResource> meshResources = GLTFLoader::LoadMeshResources(data);
+	GLTFLoader::Free(data);
+
+	std::vector<StaticMesh*> meshes;
+	for (const auto& meshResource : meshResources)
+	{
+		std::vector<StaticMesh::Vertex> vertices(meshResource.positions.size());
+		std::vector<uint32_t> indices = meshResource.indices;
+
+		for (uint32_t index = 0; index < vertices.size(); ++index)
+		{
+			vertices[index].position = meshResource.positions[index];
+			vertices[index].normal = meshResource.normals[index];
+			vertices[index].tangent = meshResource.tangents[index];
+			vertices[index].texcoord = meshResource.texcoords[index];
+		}
+
+		meshes.push_back(RenderModule::CreateResource<StaticMesh>(vertices, indices));
+	}
+
+	Shader* shader = RenderModule::CreateResource<Shader>("Resource/Shader/StaticMesh.vert", "Resource/Shader/Mesh.frag");
 
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
@@ -60,10 +92,24 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-			textRenderer->DrawText2D(font, L"Hello, World!", Vec2f(0.0f, 24.0f), EStartPivot::LEFT_TOP, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
-			textRenderer->DrawText2D(font, L"Hello, World!", Vec2f(0.0f, 48.0f), EStartPivot::LEFT_TOP, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
-			textRenderer->DrawText2D(font, L"Hello, World!", Vec2f(0.0f, 72.0f), EStartPivot::LEFT_TOP, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
-			textRenderer->DrawText2D(font, L"Hello, World!", Vec2f(0.0f, 96.0f), EStartPivot::LEFT_TOP, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+			renderer3d->DrawGrid3D(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
+
+			shader->Bind();
+			{
+				tileColorMap->Active(0);
+
+				shader->SetUniform("world", Mat4x4::Identity());
+				shader->SetUniform("view", camera->GetView());
+				shader->SetUniform("projection", camera->GetProjection());
+
+				for (const auto& mesh : meshes)
+				{
+					mesh->Bind();
+					RenderModule::ExecuteDrawIndex(mesh->GetIndexCount(), EDrawMode::TRIANGLES);
+					mesh->Unbind();
+				}
+			}
+			shader->Unbind();
 
 			RenderModule::EndFrame();
 		}
