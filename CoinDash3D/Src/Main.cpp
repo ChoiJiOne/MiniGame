30086573a0458effbd382a694e9d4a075613307a
choijione <choijione02@gmail.com>
2024-06-15 11:44:55 +0900

    필요 없는 서드 파티 라이브러리 헤더 선언 삭제

--- CoinDash3D/Src/Main.cpp
+++ CoinDash3D/Src/Main.cpp
@@ -11,10 +11,6 @@
 #include "PlatformModule.h"
 #include "RenderModule.h"
 
-
-#include <glad/glad.h>
-#include <imgui.h>
-
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
