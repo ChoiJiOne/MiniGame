cebc2b6cbc58091ab6fe75d958fbce4831507959
choijione <choijione02@gmail.com>
2024-06-16 13:06:24 +0900

    전체 게임 씬을 화면에 그리는 코드 추가

--- CoinDash3D/Src/Main.cpp
+++ CoinDash3D/Src/Main.cpp
@@ -6,10 +6,24 @@
 #endif
 
 #include "CrashModule.h"
+#include "DepthRenderer.h"
 #include "GameModule.h"
+#include "GeometryRenderer3D.h"
 #include "GLTFLoader.h"
 #include "PlatformModule.h"
+#include "MeshRenderer.h"
 #include "RenderModule.h"
+#include "Shader.h"
+#include "ShadowMap.h"
+#include "StaticMesh.h"
+
+#include "Camera.h"
+#include "Character.h"
+#include "Coin.h"
+#include "Floor.h"
+
+
+#include <glad/glad.h>
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -25,12 +39,88 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	GameModule::Init();
 
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
+
+	GeometryRenderer3D* renderer3d = RenderModule::CreateResource<GeometryRenderer3D>();
+	DepthRenderer* depthRenderer = RenderModule::CreateResource<DepthRenderer>();
+	MeshRenderer* meshRenderer = RenderModule::CreateResource<MeshRenderer>();
+	ShadowMap* shadowMap = RenderModule::CreateResource<ShadowMap>(ShadowMap::ESize::Size_2048);
 	
+	Coin* coin = GameModule::CreateEntity<Coin>(Vec3f(0.0f, 0.5f, 0.0f));
+	Floor* floor = GameModule::CreateEntity<Floor>();
+	Character* character = GameModule::CreateEntity<Character>();
+	Camera* camera = GameModule::CreateEntity<Camera>(character);
+
+	Vec3f lightPosition = Vec3f(5.0f, 5.0f, 5.0f);
+	Mat4x4 lightProjection = Mat4x4::Ortho(-4.0f, +4.0f, -4.0f, +4.0f, 1.0f, 40.0f);
+	Mat4x4 lightView = Mat4x4::LookAt(lightPosition, Vec3f(0.0f, 0.0f, 0.0f), Vec3f(0.0f, 1.0f, 0.0f));
+	Mat4x4 lightSpaceMatrix = lightView * lightProjection;
+
+		
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
+			character->Tick(deltaSeconds);
+			camera->Tick(deltaSeconds);
+			coin->Tick(deltaSeconds);
+			floor->Tick(deltaSeconds);
+
+			renderer3d->SetView(camera->GetView());
+			renderer3d->SetProjection(camera->GetProjection());
+
+			shadowMap->Bind();
+			{
+				shadowMap->Clear();
+				RenderModule::SetViewport(0, 0, shadowMap->GetSize(), shadowMap->GetSize());
+
+				depthRenderer->SetLightSpaceMatrix(lightSpaceMatrix);
+
+				const std::vector<StaticMesh*>& meshes0 = floor->GetMeshes();
+				for (const auto& mesh : meshes0)
+				{
+					depthRenderer->DrawStaticMesh(Mat4x4::Identity(), mesh);
+				}
+
+				const std::vector<StaticMesh*>& meshes1 = coin->GetMeshes();
+				for (const auto& mesh : meshes1)
+				{
+					depthRenderer->DrawStaticMesh(Mat4x4::Identity(), mesh);
+				}
+
+				const std::vector<SkinnedMesh*>& skinnedMeshes = character->GetMeshes();
+				for (const auto& mesh : skinnedMeshes)
+				{
+					depthRenderer->DrawSkinnedMesh(Transform::ToMat(character->GetTransform()), character->GetBindPose(), character->GetInvBindPose(), mesh);
+				}
+			}
+			shadowMap->Unbind();
+
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
+			meshRenderer->SetShadowMap(shadowMap);
+			meshRenderer->SetView(camera->GetView());
+			meshRenderer->SetProjection(camera->GetProjection());
+			meshRenderer->SetLightSpaceMatrix(lightSpaceMatrix);
+			meshRenderer->SetLightPosition(lightPosition);
+			meshRenderer->SetCameraPosition(camera->GetEyePosition());
+
+			const std::vector<StaticMesh*>& meshes0 = floor->GetMeshes();
+			for (const auto& mesh : meshes0)
+			{
+				meshRenderer->DrawStaticMesh(Mat4x4::Identity(), mesh, floor->GetMaterial());
+			}
+
+			const std::vector<StaticMesh*>& meshes1 = coin->GetMeshes();
+			for (const auto& mesh : meshes1)
+			{
+				meshRenderer->DrawStaticMesh(Mat4x4::Identity(), mesh, floor->GetMaterial());
+			}
+
+			const std::vector<SkinnedMesh*>& skinnedMeshes = character->GetMeshes();
+			for (const auto& mesh : skinnedMeshes)
+			{
+				meshRenderer->DrawSkinnedMesh(Transform::ToMat(character->GetTransform()), character->GetBindPose(), character->GetInvBindPose(), mesh, character->GetMaterial());
+			}
+			
 			RenderModule::EndFrame();
 		}
 	);
