7729b7feeddd323a83f741c066a3cd466150977b
choijione <choijione02@gmail.com>
2024-06-16 13:05:28 +0900

    캐릭터 엔티티 추가

---
+++ CoinDash3D/Inc/Character.h
@@ -0,0 +1,196 @@
+#pragma once
+
+#include "CrossFadeController.h"
+#include "Collision.h"
+#include "IEntity.h"
+#include "ITexture.h"
+#include "Skeleton.h"
+#include "SkinnedMesh.h"
+
+
+/**
+ * @brief 캐릭터 엔티티입니다.
+ */
+class Character : public IEntity
+{
+public:
+	/**
+	 * @brief 캐릭터의 상태입니다.
+	 */
+	enum class EStatus
+	{
+		IDLE = 0x00, // 대기중
+		RUN = 0x01, // 달리는 중
+	};
+
+
+public:
+	/**
+	 * @brief 캐릭터 엔티티의 생성자입니다.
+	 */
+	Character();
+
+
+	/**
+	 * @brief 캐릭터 엔티티의 가상 소멸자입니다.
+	 */
+	virtual ~Character();
+
+
+	/**
+	 * @brief 캐릭터 엔티티의 복사 생성자 및 대입 연산자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Character);
+
+
+	/**
+	 * @brief 캐릭터 엔티티를 업데이트합니다.
+	 *
+	 * @param deltaSeconds 델타 시간 값입니다.
+	 */
+	virtual void Tick(float deltaSeconds);
+
+
+	/**
+	 * @brief 캐릭터 엔티티 내의 리소스를 할당 해제합니다.
+	 */
+	virtual void Release();
+
+
+	/**
+	 * @brief 캐릭터의 메시 리소스 목록을 얻습니다.
+	 *
+	 * @return 캐릭터의 메시 리소스 목록 참조자를 반환합니다.
+	 */
+	const std::vector<SkinnedMesh*>& GetMeshes() const { return meshes_; }
+
+
+	/**
+	 * @brief 캐릭터의 메터리얼(재질)을 얻습니다.
+	 *
+	 * @return 캐릭터의 메터리얼(재질) 참조자를 반환합니다.
+	 */
+	const ITexture* GetMaterial() const { return material_; }
+
+
+	/**
+	 * @brief 캐릭터의 경계 영역을 얻습니다.
+	 *
+	 * @return 캐릭터의 경계 영역 참조자를 반환합니다.
+	 */
+	const Sphere& GetSphere() const { return sphere_; }
+
+
+	/**
+	 * @brief 캐릭터의 트랜스폼을 얻습니다.
+	 *
+	 * @return 캐릭터의 트랜스폼 참조자를 반환합니다.
+	 */
+	const Transform& GetTransform() const { return transform_; }
+
+
+	/**
+	 * @brief 캐릭터의 크로스 페이드 컨트롤러를 얻습니다.
+	 *
+	 * @return 캐릭터의 크로스 페이드 컨트롤러 참조자를 반환합니다.
+	 */
+	CrossFadeController& GetCrossFadeController() { return crossFadeController_; }
+
+
+	/**
+	 * @brief 캐릭터의 바인드 포즈 행렬을 얻습니다.
+	 * 
+	 * @return 캐릭터의 바인드 포즈 행렬을 반환합니다.
+	 */
+	const std::vector<Mat4x4>& GetBindPose() const { return bindPose_; }
+
+
+	/**
+	 * @brief 캐릭터의 역 바인드 포즈 행렬을 얻습니다.
+	 *
+	 * @return 캐릭터의 역 바인드 포즈 행렬을 반환합니다.
+	 */
+	const std::vector<Mat4x4>& GetInvBindPose() { return crossFadeController_.GetSkeleton().GetInvBindPose(); }
+
+
+private:
+	/**
+	 * @brief 캐릭터의 이동 방향을 얻습니다.
+	 *
+	 * @return 캐릭터의 이동 방향을 반환합니다.
+	 */
+	Vec3f GetMoveDirection();
+
+
+private:
+	/**
+	 * @brief 스키닝 메시 리소스입니다.
+	 */
+	std::vector<SkinnedMesh*> meshes_;
+
+
+	/**
+	 * @brief 캐릭터의 메터리얼입니다.
+	 */
+	ITexture* material_ = nullptr;
+
+
+	/**
+	 * @brief 캐릭터의 경계 영역입니다.
+	 */
+	Sphere sphere_;
+
+
+	/**
+	 * @brief 캐릭터의 트랜스폼입니다.
+	 */
+	Transform transform_;
+
+
+	/**
+	 * @brief 대기 애니메이션 클립 인덱스입니다.
+	 */
+	uint32_t idleClip_ = 0;
+
+
+	/**
+	 * @brief 달리기 애니메이션 클립 인덱스입니다.
+	 */
+	uint32_t runClip_ = 0;
+
+
+	/**
+	 * @brief 애니메이션 클립입니다.
+	 */
+	std::vector<Clip> clips_;
+
+
+	/**
+	 * @brief 캐릭터 애니메이션의 뼈대입니다.
+	 */
+	Skeleton skeleton_;
+
+
+	/**
+	 * @brief 클립의 부드러운 전환을 위한 크로스 페이드 컨트롤러입니다.
+	 */
+	CrossFadeController crossFadeController_;
+
+
+	/**
+	 * @brief 바인드 포즈 행렬입니다.
+	 */
+	std::vector<Mat4x4> bindPose_;
+
+
+	/**
+	 * @brief 캐릭터의 현재 상태입니다.
+	 */
+	EStatus currentStatus_ = EStatus::IDLE;
+
+
+	/**
+	 * @brief 캐릭터의 이동 속도입니다.
+	 */
+	float moveSpeed_ = 0.0f;
+};
---
+++ CoinDash3D/Src/Character.cpp
@@ -0,0 +1,220 @@
+#include <array>
+
+#include <cgltf.h>
+
+#include "GLTFLoader.h"
+#include "InputController.h"
+#include "RenderModule.h"
+#include "SkinnedMesh.h"
+#include "Texture2D.h"
+
+#include "Camera.h"
+#include "Character.h"
+
+Character::Character()
+{
+	static std::vector<SkinnedMesh*> meshes;
+	static Skeleton skeleton;
+	static std::vector<Clip> clips;
+
+	if (meshes.empty())
+	{
+		cgltf_data* data = GLTFLoader::Load("Resource/Model/Michelle.glb");
+
+		std::vector<GLTFLoader::MeshResource> meshResources = GLTFLoader::LoadMeshResources(data);
+		skeleton = GLTFLoader::LoadSkeleton(data);
+		clips = GLTFLoader::LoadAnimationClips(data);
+		GLTFLoader::Free(data);
+
+		for (const auto& meshResource : meshResources)
+		{
+			std::vector<SkinnedMesh::Vertex> vertices(meshResource.positions.size());
+			std::vector<uint32_t> indices = meshResource.indices;
+
+			for (uint32_t index = 0; index < vertices.size(); ++index)
+			{
+				vertices[index].position = meshResource.positions[index];
+				vertices[index].normal = meshResource.normals[index];
+				//vertices[index].tangent = meshResource.tangents[index];
+				vertices[index].texcoord = meshResource.texcoords[index];
+				vertices[index].weight = meshResource.weights[index];
+				vertices[index].joints = meshResource.joints[index];
+			}
+
+			meshes_.push_back(RenderModule::CreateResource<SkinnedMesh>(vertices, indices));
+		}
+	}
+
+	static Texture2D* material = nullptr;
+	if (!material)
+	{
+		material = RenderModule::CreateResource<Texture2D>("Resource/Texture/Michelle.png", false);
+	}
+
+	material_ = material;
+	sphere_ = Sphere(transform_.position + Vec3f(0.0f, 0.7f, 0.0f), 0.3f);
+	transform_ = Transform();
+
+	skeleton_ = skeleton;
+	clips_ = clips;
+
+	for (uint32_t index = 0; index < clips_.size(); ++index)
+	{
+		if (clips_[index].GetName() == "IDLE")
+		{
+			idleClip_ = index;
+		}
+		else if (clips_[index].GetName() == "RUN")
+		{
+			runClip_ = index;
+		}
+	}
+
+	crossFadeController_.SetSkeleton(skeleton_);
+	crossFadeController_.Play(&clips_[idleClip_]);
+	moveSpeed_ = 5.0f;
+}
+
+Character::~Character()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Character::Tick(float deltaSeconds)
+{
+	crossFadeController_.Update(deltaSeconds);
+	crossFadeController_.GetCurrentPose().GetMatrixPalette(bindPose_);
+
+	static std::array<EKey, 4> keys =
+	{
+		EKey::KEY_A,
+		EKey::KEY_D,
+		EKey::KEY_W,
+		EKey::KEY_S,
+	};
+
+	// 하나라도, Press면 달리기 수행
+	for (const auto& key : keys)
+	{
+		EPressState state = InputController::GetKeyPressState(key);
+
+		if (state == EPressState::PRESSED && currentStatus_ == EStatus::IDLE)
+		{
+			currentStatus_ = EStatus::RUN;
+			crossFadeController_.FadeTo(&clips_[runClip_], 0.1f);
+			break;
+		}
+	}
+
+	// 모두 Released면 대기 수행
+	bool bChangeClip = true;
+	for (const auto& key : keys)
+	{
+		EPressState state = InputController::GetKeyPressState(key);
+
+		if (state != EPressState::RELEASED && state != EPressState::NONE) // 모두 Released면 대기 수행
+		{
+			bChangeClip = false;
+			break;
+		}
+	}
+
+	if (bChangeClip && currentStatus_ == EStatus::RUN)
+	{
+		currentStatus_ = EStatus::IDLE;
+		crossFadeController_.FadeTo(&clips_[idleClip_], 0.1f);
+	}
+
+	Vec3f direction = GetMoveDirection();
+	float rotateRadian = Vec3f::Radian(direction, Vec3f(0.0f, 0.0f, 1.0f));
+	rotateRadian = (direction.x > 0.0f) ? rotateRadian : TwoPi - rotateRadian;
+
+	transform_.rotate = Quat::AxisRadian(Vec3f(0.0f, 1.0f, 0.0f), rotateRadian);
+
+	if (currentStatus_ == EStatus::RUN)
+	{
+		Vec3f position = transform_.position;
+
+		float sin = MathModule::Sin(rotateRadian);
+		float cos = MathModule::Cos(rotateRadian);
+
+		position += Vec3f(deltaSeconds * moveSpeed_ * sin, 0.0f, deltaSeconds * moveSpeed_ * cos);
+		sphere_.center = position + Vec3f(0.0f, 0.7f, 0.0f);
+
+		//const std::vector<AABB>& aabbs = wall_->GetBoundingBoxes();
+		//bool bIsCollision = false;
+		//for (const auto& aabb : aabbs)
+		//{
+		//	if (Collision::SphereToAABB(sphere_, aabb))
+		//	{
+		//		bIsCollision = true;
+		//		break;
+		//	}
+		//}
+
+		//if (bIsCollision)
+		//{
+		//	position = transform_.position;
+		//	sphere_.center = position + Vec3f(0.0f, 0.7f, 0.0f);
+		//}
+
+		transform_.position = position;
+	}
+}
+
+void Character::Release()
+{
+	if (bIsInitialized_)
+	{
+		bIsInitialized_ = false;
+	}
+}
+
+Vec3f Character::GetMoveDirection()
+{
+	Vec3f direction;
+	bool bIsUpdate = false;
+
+	if (InputController::GetKeyPressState(EKey::KEY_A) == EPressState::HELD)
+	{
+		bIsUpdate = true;
+		direction += Vec3f(-1.0f, 0.0f, 0.0f);
+	}
+
+	if (InputController::GetKeyPressState(EKey::KEY_D) == EPressState::HELD)
+	{
+		bIsUpdate = true;
+		direction += Vec3f(+1.0f, 0.0f, 0.0f);
+	}
+
+	if (InputController::GetKeyPressState(EKey::KEY_W) == EPressState::HELD)
+	{
+		bIsUpdate = true;
+		direction += Vec3f(0.0f, 0.0f, -1.0f);
+	}
+
+	if (InputController::GetKeyPressState(EKey::KEY_S) == EPressState::HELD)
+	{
+		bIsUpdate = true;
+		direction += Vec3f(0.0f, 0.0f, +1.0f);
+	}
+
+	if (bIsUpdate && direction != Vec3f(0.0f, 0.0f, 0.0f))
+	{
+		currentStatus_ = EStatus::RUN;
+		direction = Vec3f::Normalize(direction);
+	}
+	else
+	{
+		float rotate = Quat::Radian(transform_.rotate);
+
+		direction.x = MathModule::Sin(rotate);
+		direction.y = 0.0f;
+		direction.z = MathModule::Cos(rotate);
+	}
+
+	return direction;
+}
