7320a34197b97cecc583507cc50ca239d698efb4
choijione <choijione02@gmail.com>
2024-06-25 18:13:46 +0900

    2D 렌더링을 수행하는 렌더러 추가

---
+++ Engine/Module/RenderModule/Inc/Renderer2D.h
@@ -0,0 +1,273 @@
+#pragma once
+
+#include <array>
+
+#include "RenderModule.h"
+#include "VertexBuffer.h"
+
+class Shader;
+
+
+/**
+ * @brief 2D 렌더링을 수행하는 렌더러입니다.
+ * 
+ * @note 좌표계 기준은 윈도우 좌표계 기준입니다.
+ */
+class Renderer2D : public IResource
+{
+public:
+	/**
+	 * @brief 2D 렌더러의 생성자입니다.
+	 */
+	explicit Renderer2D();
+
+
+	/**
+	 * @brief 2D 렌더러의 가상 소멸자입니다.
+	 */
+	virtual ~Renderer2D();
+
+
+	/**
+	 * @brief 2D 렌더러의 대입 연산자 및 복사 생성자를 명시적으로 삭제합니다.
+	 */
+	DISALLOW_COPY_AND_ASSIGN(Renderer2D);
+
+
+	/**
+	 * @brief 렌더러 내부 리소스를 할당 해제합니다.
+	 */
+	virtual void Release() override;
+
+
+	/**
+	 * @brief 렌더러의 직교 투영 행렬을 설정합니다.
+	 *
+	 * @param ortho 설정할 직교 투영 행렬입니다.
+	 */
+	void SetOrtho(const Mat4x4& ortho) { ortho_ = ortho; }
+
+
+	/**
+	 * @brief 2D 점을 그립니다.
+	 * 
+	 * @param positions 점의 배열 포인터입니다.
+	 * @param colors 색상 배열 포인터입니다. 이때, 점과 색상의 수는 같아야 합니다.
+	 * @param size 점의 개수입니다.
+	 * @param pointSize 점의 크기입니다. 기본 값은 1.0f 입니다.
+	 */
+	void DrawPoint(const Vec2f* positions, const Vec4f* colors, uint32_t size, float pointSize = 1.0f);
+
+	
+private:
+	/**
+	 * @brief 2D 렌더러 내부에서 사용하는 정점입니다.
+	 */
+	struct Vertex
+	{
+		/**
+		 * @brief 정점의 기본 생성자입니다.
+		 */
+		Vertex() noexcept
+			: position(0.0f, 0.0f)
+			, uv(0.0f, 0.0f)
+			, color(0.0f, 0.0f, 0.0f, 0.0f) {}
+
+
+		/**
+		 * @brief 정점의 생성자입니다.
+		 *
+		 * @param p 정점의 위치입니다.
+		 * @param t 정점의 텍스처 좌표입니다.
+		 * @param c 정점의 색상입니다.
+		 */
+		Vertex(Vec2f&& p, Vec2f& t, Vec4f&& c) noexcept
+			: position(p)
+			, uv(t)
+			, color(c) {}
+
+
+		/**
+		 * @brief 정점의 생성자입니다.
+		 *
+		 * @param p 정점의 위치입니다.
+		 * @param t 정점의 텍스처 좌표입니다.
+		 * @param c 정점의 색상입니다.
+		 */
+		Vertex(const Vec2f& p, const Vec2f& t, const Vec4f& c) noexcept
+			: position(p)
+			, uv(t)
+			, color(c) {}
+
+
+		/**
+		 * @brief 정점의 생성자입니다.
+		 *
+		 * @param x 정점 위치의 X좌표입니다.
+		 * @param y 정점 위치의 Y좌표입니다.
+		 * @param u 정점 텍스처의 U좌표입니다.
+		 * @param v 정점 텍스처의 V좌표입니다.
+		 * @param r 정점 색상의 R입니다.
+		 * @param g 정점 색상의 G입니다.
+		 * @param b 정점 색상의 B입니다.
+		 * @param a 정점 색상의 A입니다.
+		 */
+		Vertex(
+			float x, float y,
+			float u, float v,
+			float r, float g, float b, float a
+		) noexcept
+			: position(x, y)
+			, uv(u, v)
+			, color(r, g, b, a) {}
+
+
+		/**
+		 * @brief 정점의 생성자입니다.
+		 *
+		 * @param instance 복사할 정점 인스턴스입니다.
+		 */
+		Vertex(Vertex&& instance) noexcept
+			: position(instance.position)
+			, uv(instance.uv)
+			, color(instance.color) {}
+
+
+		/**
+		 * @brief 정점의 생성자입니다.
+		 *
+		 * @param instance 복사할 정점 인스턴스입니다.
+		 */
+		Vertex(const Vertex& instance) noexcept
+			: position(instance.position)
+			, uv(instance.uv)
+			, color(instance.color) {}
+
+
+		/**
+		 * @brief 정점의 대입 연산자입니다.
+		 *
+		 * @param instance 대입할 정점 인스턴스입니다.
+		 *
+		 * @return 대입한 객체의 참조자를 반환합니다.
+		 */
+		Vertex& operator=(Vertex&& instance) noexcept
+		{
+			if (this == &instance) return *this;
+
+			position = instance.position;
+			uv = instance.uv;
+			color = instance.color;
+
+			return *this;
+		}
+
+
+		/**
+		 * @brief 정점의 대입 연산자입니다.
+		 *
+		 * @param instance 대입할 정점 인스턴스입니다.
+		 *
+		 * @return 대입한 객체의 참조자를 반환합니다.
+		 */
+		Vertex& operator=(const Vertex& instance) noexcept
+		{
+			if (this == &instance) return *this;
+
+			position = instance.position;
+			uv = instance.uv;
+			color = instance.color;
+
+			return *this;
+		}
+
+
+		/**
+		 * @brief 정점의 바이트 보폭 값을 얻습니다.
+		 *
+		 * @return 정점의 파이트 보폭(Stride) 값을 반환합니다.
+		 */
+		static uint32_t GetStride()
+		{
+			return sizeof(Vertex);
+		}
+
+
+		/**
+		 * @brief 정점의 위치입니다.
+		 */
+		Vec2f position;
+
+
+		/**
+		 * @brief 정점의 텍스처 좌표입니다.
+		 */
+		Vec2f uv;
+
+
+		/**
+		 * @brief 정점의 색상입니다.
+		 */
+		Vec4f color;
+	};
+
+
+private:
+	/**
+	 * @brief 그리기를 수행합니다.
+	 * 
+	 * @param transform 변환 행렬입니다.
+	 * @param drawMode 그리기 모드입니다.
+	 * @param vertexCount 정점 수입니다.
+	 */
+	void Draw(const Mat4x4& transform, const EDrawMode& drawMode, uint32_t vertexCount);
+
+
+private:
+	/**
+	 * @brief 도형 정점 목록의 최대 크기입니다.
+	 */
+	static const int32_t MAX_VERTEX_SIZE = 10000;
+
+
+	/**
+	 * @brief 곡선 계산 시 최대 분할 횟수입니다.
+	 */
+	static const int32_t MAX_SLICE_SIZE = 7;
+
+
+	/**
+	 * @brief 도형의 정점 목록입니다.
+	 */
+	std::array<Vertex, MAX_VERTEX_SIZE> vertices_;
+
+
+	/**
+	 * @brief 렌더러의 직교 투영 행렬입니다.
+	 */
+	Mat4x4 ortho_;
+
+
+	/**
+	 * @brief 점의 크기입니다.
+	 */
+	float pointSize_ = 1.0f;
+
+
+	/**
+	 * @brief 정점 버퍼 목록의 오브젝트입니다.
+	 */
+	uint32_t vertexArrayObject_ = 0;
+
+
+	/**
+	 * @brief 정점 버퍼입니다.
+	 */
+	VertexBuffer* vertexBuffer_ = nullptr;
+
+
+	/**
+	 * @brief 렌더링 시 사용할 셰이더입니다.
+	 */
+	Shader* shader_ = nullptr;
+};
---
+++ Engine/Module/RenderModule/Src/Renderer2D.cpp
@@ -0,0 +1,121 @@
+#pragma warning(push)
+#pragma warning(disable: 26495)
+#pragma warning(disable: 26819)
+#pragma warning(disable: 26451)
+
+#include <glad/glad.h>
+
+#include "GLAssertion.h"
+#include "Renderer2D.h"
+#include "Shader.h"
+
+
+#pragma warning(pop)
+
+Renderer2D::Renderer2D()
+{
+	std::string vsPath = "Resource/Shader/Renderer2D.vert";
+	std::string fsPath = "Resource/Shader/Renderer2D.frag";
+	shader_ = RenderModule::CreateResource<Shader>(vsPath, fsPath);
+
+	uint32_t byteSize = static_cast<uint32_t>(Vertex::GetStride() * vertices_.size());
+	VertexBuffer::EUsage usage = VertexBuffer::EUsage::DYNAMIC;
+	vertexBuffer_ = RenderModule::CreateResource<VertexBuffer>(byteSize, usage);
+
+	GL_FAILED(glGenVertexArrays(1, &vertexArrayObject_));
+	GL_FAILED(glBindVertexArray(vertexArrayObject_));
+	{
+		vertexBuffer_->Bind();
+
+		GL_FAILED(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, Vertex::GetStride(), (void*)(offsetof(Vertex, position))));
+		GL_FAILED(glEnableVertexAttribArray(0));
+
+		GL_FAILED(glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, Vertex::GetStride(), (void*)(offsetof(Vertex, uv))));
+		GL_FAILED(glEnableVertexAttribArray(1));
+
+		GL_FAILED(glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, Vertex::GetStride(), (void*)(offsetof(Vertex, color))));
+		GL_FAILED(glEnableVertexAttribArray(2));
+
+		vertexBuffer_->Unbind();
+	}
+	GL_FAILED(glBindVertexArray(0));
+
+
+	bIsInitialized_ = true;
+}
+
+Renderer2D::~Renderer2D()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Renderer2D::Release()
+{
+	CHECK(bIsInitialized_);
+
+	if (shader_)
+	{
+		RenderModule::DestroyResource(shader_);
+		shader_ = nullptr;
+	}
+
+	if (vertexBuffer_)
+	{
+		RenderModule::DestroyResource(vertexBuffer_);
+		vertexBuffer_ = nullptr;
+	}
+
+	GL_FAILED(glDeleteVertexArrays(1, &vertexArrayObject_));
+
+	bIsInitialized_ = false;
+}
+
+void Renderer2D::DrawPoint(const Vec2f* positions, const Vec4f* colors, uint32_t size, float pointSize)
+{
+	CHECK(size <= MAX_VERTEX_SIZE);
+	CHECK(pointSize >= 0.0f);
+
+	for (uint32_t index = 0; index < size; ++index)
+	{
+		vertices_[index].position = Vec2f(positions[index]);
+		vertices_[index].color = colors[index];
+	}
+
+	pointSize_ = pointSize;
+	Draw(Mat4x4::Identity(), EDrawMode::POINTS, size);
+}
+
+void Renderer2D::Draw(const Mat4x4& transform, const EDrawMode& drawMode, uint32_t vertexCount)
+{
+	CHECK(drawMode != EDrawMode::NONE);
+
+	GLboolean originEnableDepth;
+	GL_FAILED(glGetBooleanv(GL_DEPTH_TEST, &originEnableDepth));
+
+	RenderModule::SetDepthMode(false);
+	{
+		shader_->Bind();
+
+		const void* vertexPtr = reinterpret_cast<const void*>(vertices_.data());
+		uint32_t bufferByteSize = static_cast<uint32_t>(Vertex::GetStride() * vertices_.size());
+		vertexBuffer_->SetBufferData(vertexPtr, bufferByteSize);
+
+		shader_->SetUniform("transform", transform);
+		shader_->SetUniform("ortho", ortho_);
+
+		if (drawMode == EDrawMode::POINTS)
+		{
+			shader_->SetUniform("pointSize", pointSize_);
+		}
+
+		GL_FAILED(glBindVertexArray(vertexArrayObject_));
+		RenderModule::ExecuteDrawVertex(vertexCount, drawMode);
+		GL_FAILED(glBindVertexArray(0));
+
+		shader_->Unbind();
+	}
+	RenderModule::SetDepthMode(originEnableDepth);
+}
