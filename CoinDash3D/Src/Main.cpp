ff6e8b93cac5a8c43bd3f201f86a3e12d39f4031
choijione <choijione02@gmail.com>
2024-06-28 17:57:14 +0900

    아무 동작도 수행하지 않는 게임 루프 추가

--- CoinDash3D/Src/Main.cpp
+++ CoinDash3D/Src/Main.cpp
@@ -5,11 +5,35 @@
 #include <crtdbg.h>
 #endif
 
+#include <SDL2/SDL.h>
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
+
+	SDL_Init(SDL_INIT_EVERYTHING);
 	
+	SDL_Window* window = nullptr;
+	window = SDL_CreateWindow("CoinDash3D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+
+	SDL_Event e;
+	bool quit = false;
+	while (!quit)
+	{
+		while (SDL_PollEvent(&e))
+		{
+			if (e.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	window = nullptr;
+
+	SDL_Quit();
 	return 0;
 }
