04c5ebfa70d91f518304a7f673d810c634a7952a
choijione <choijione02@gmail.com>
2024-06-28 17:45:57 +0900

    빌드 테스트를 위한 빈 메인 함수 추가

---
+++ CoinDash3D/Src/Main.cpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include <Windows.h>
+
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+#include <crtdbg.h>
+#endif
+
+int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
+{
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+	
+	return 0;
+}
