76161adc8db24dd964da0cfcfaa8786ed3d11c81
choijione <choijione02@gmail.com>
2024-06-29 07:30:55 +0900

    타입(클래스 혹은 구조체)의 디폴트 생성자와 아무 동작도 수행하지 않는 가상 소멸자를 삽입하는 매크로 추가

--- GameMaker/Core/Inc/Macro.h
+++ GameMaker/Core/Inc/Macro.h
@@ -15,3 +15,15 @@ TypeName(const TypeName&) = delete;           \
 TypeName& operator=(TypeName&&) = delete;     \
 TypeName& operator=(const TypeName) = delete;
 #endif
+
+
+/**
+ * @brief 타입(클래스 혹은 구조체)의 디폴트 생성자와 아무 동작도 수행하지 않는 가상 소멸자를 선언합니다.
+ *
+ * @param TypeName 디폴트 생성자와 가상 소멸자를 생성할 타입의 이름입니다.
+ */
+#ifndef DEFAULT_CONSTRUCTOR_AND_VIRTUAL_DESTRUCTOR
+#define DEFAULT_CONSTRUCTOR_AND_VIRTUAL_DESTRUCTOR(TypeName)\
+TypeName() = default;\
+virtual ~TypeName() {}
+#endif
