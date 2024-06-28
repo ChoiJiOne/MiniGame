14f442dada4b5473b150586332820ccfff5aade4
choijione <choijione02@gmail.com>
2024-06-28 11:02:02 +0900

    변경 사항이 적용된 2D/3D 렌더러의 테스트 코드 추가

--- Impl/Src/Main.cpp
+++ Impl/Src/Main.cpp
@@ -10,12 +10,16 @@
 
 #include "Assertion.h"
 #include "CrashModule.h"
+#include "GameModule.h"
 #include "PlatformModule.h"
 #include "RenderModule.h"
 
 #include "Mat.h"
 #include "Renderer2D.h"
+#include "Renderer3D.h"
 #include "TTFont.h"
+
+#include "Camera.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -31,64 +35,31 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 
 	PlatformModule::SetEndLoopCallback([&]() { RenderModule::Uninit(); });
 
-	TTFont* font = RenderModule::CreateResource<TTFont>("Resource/Font/SeoulNamsanEB.ttf", 0x0, 0x127, 32.0f);
-	Renderer2D* renderer = RenderModule::CreateResource<Renderer2D>();
-	
-	std::vector<Vec2f> positions;
-	std::vector<Vec4f> colors;
+	TTFont* font = RenderModule::CreateResource<TTFont>("Impl/Resource/Font/SeoulNamsanEB.ttf", 0x0, 0x127, 32.0f);
+	Renderer2D* renderer2d = RenderModule::CreateResource<Renderer2D>();
+	Renderer3D* renderer3d = RenderModule::CreateResource<Renderer3D>();
 
-	for (float x = -400.0f; x <= 400.0f; x += 10.0f)
-	{
-		positions.push_back(Vec2f(x, -300.0f));
-		positions.push_back(Vec2f(x, +300.0f));
-
-		if (MathModule::NearZero(x))
-		{
-			colors.push_back(Vec4f(1.0f, 0.0f, 0.0f, 0.5f));
-			colors.push_back(Vec4f(1.0f, 0.0f, 0.0f, 0.5f));
-		}
-		else
-		{
-			colors.push_back(Vec4f(1.0f, 1.0f, 1.0f, 0.5f));
-			colors.push_back(Vec4f(1.0f, 1.0f, 1.0f, 0.5f));
-		}
-	}
-
-	for (float y = -300.0f; y <= 300.0f; y += 10)
-	{
-		positions.push_back(Vec2f(-400.0f, y));
-		positions.push_back(Vec2f(+400.0f, y));
-
-		if (MathModule::NearZero(y))
-		{
-			colors.push_back(Vec4f(0.0f, 0.0f, 1.0f, 0.5f));
-			colors.push_back(Vec4f(0.0f, 0.0f, 1.0f, 0.5f));
-		}
-		else
-		{
-			colors.push_back(Vec4f(1.0f, 1.0f, 1.0f, 0.5f));
-			colors.push_back(Vec4f(1.0f, 1.0f, 1.0f, 0.5f));
-		}
-	}
-
-	int32_t width = 0;
-	int32_t height = 0;
-	RenderModule::GetScreenSize(width, height);
+	Camera* camera = GameModule::CreateEntity<Camera>();
 
 	PlatformModule::RunLoop(
 		[&](float deltaSeconds)
 		{
-			renderer->SetOrtho(Mat4x4::Ortho(-width * 0.5f, +width * 0.5f, -height * 0.5f, +height * 0.5f, -1.0f, 1.0f));
+			camera->Tick(deltaSeconds);
 
 			RenderModule::BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 
-			renderer->DrawLines(positions.data(), colors.data(), positions.size());
-			renderer->DrawLine(Vec2f(-200.0f, -200.0f), Vec2f(+200.0f, +200.0f), Vec4f(1.0f, 1.0f, 0.0f, 1.0f));
-			//renderer->DrawCircleWireframe(Vec2f(400.3f, 300.0f), 50.3f, Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
-			//renderer->DrawEllipse(Vec2f(400.3f, 300.0f), 50.0f, 200.0f, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
-			//renderer->DrawEllipseWireframe(Vec2f(400.3f, 300.0f), 50.0f, 200.0f, Vec4f(1.0f, 0.0f, 1.0f, 1.0f));
+			renderer3d->Begin(camera->GetView(), camera->GetProjection());
+			{
+				renderer3d->DrawGrid(Vec3f(100.0f, 100.0f, 100.0f), 1.0f);
+				renderer3d->DrawSphere(Mat4x4::Identity(), 1.0f, Vec4f(1.0f, 0.0f, 1.0f, 1.0f));
+			}
+			renderer3d->End();
 
-			//renderer->DrawString(font, L"Hello, World", Vec2f(100.0f, 100.0f), Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+			renderer2d->Begin(camera->GetScreenOrtho());
+			{
+				renderer2d->DrawRoundRectWireframe(Vec2f(0.0f, 0.0f), 100.0f, 100.0f, 10.0f, Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+			}
+			renderer2d->End();
 
 			RenderModule::EndFrame();
 		}
