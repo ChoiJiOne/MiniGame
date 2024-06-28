82967c90ae2647596a39f122bc048d4e6aeeb14e
choijione <choijione02@gmail.com>
2024-06-29 08:33:33 +0900

    GameMath의 네임스페이스 내에 들여쓰기 수정

--- GameMaker/Core/Inc/GameMath.h
+++ GameMaker/Core/Inc/GameMath.h
@@ -8,365 +8,365 @@
 
 namespace GameMaker
 {
-	/**
-	 * @brief 파이(π) 값입니다.
-	 */
-	const float PI = 3.141592654f;
+/**
+ * @brief 파이(π) 값입니다.
+ */
+const float PI = 3.141592654f;
 
 
-	/**
-	 * @brief 2파이(2π) 값입니다.
-	 */
-	const float TWO_PI = 6.283185307f;
+/**
+ * @brief 2파이(2π) 값입니다.
+ */
+const float TWO_PI = 6.283185307f;
 
 
-	/**
-	 * @brief 파이(π) 의 역수 값입니다.
-	 */
-	const float ONE_DIV_PI = 0.318309886f;
+/**
+ * @brief 파이(π) 의 역수 값입니다.
+ */
+const float ONE_DIV_PI = 0.318309886f;
 
 
-	/**
-	 * @brief 2파이(2π) 의 역수 값입니다.
-	 */
-	const float ONE_DIV_2PI = 0.159154943f;
+/**
+ * @brief 2파이(2π) 의 역수 값입니다.
+ */
+const float ONE_DIV_2PI = 0.159154943f;
 
 
-	/**
-	 * @brief 파이(π) 의 반값(π/2)입니다.
-	 */
-	const float PI_DIV_2 = 1.570796327f;
+/**
+ * @brief 파이(π) 의 반값(π/2)입니다.
+ */
+const float PI_DIV_2 = 1.570796327f;
 
 
-	/**
-	 * @brief 파이(π) 의 반의 반 값(π/4)입니다.
-	 */
-	const float PI_DIV_4 = 0.785398163f;
+/**
+ * @brief 파이(π) 의 반의 반 값(π/4)입니다.
+ */
+const float PI_DIV_4 = 0.785398163f;
 
 
-	/**
-	 * @brief 엡실론(ε) 값입니다.
-	 */
-	const float EPSILON = 1.192092896e-07F;
+/**
+ * @brief 엡실론(ε) 값입니다.
+ */
+const float EPSILON = 1.192092896e-07F;
 
 
-	/**
-	 * @brief 라디안 각을 육십분법 각으로 변환합니다.
-	 *
-	 * @param radian 변환할 라디안 각입니다.
-	 *
-	 * @return 변환된 육십분법 각입니다.
-	 */
-	__forceinline float ToDegree(float radian)
+/**
+ * @brief 라디안 각을 육십분법 각으로 변환합니다.
+ *
+ * @param radian 변환할 라디안 각입니다.
+ *
+ * @return 변환된 육십분법 각입니다.
+ */
+__forceinline float ToDegree(float radian)
+{
+	return (radian * 180.0f) / PI;
+}
+
+
+/**
+ * @brief 육십분법 각을 라디안 각으로 변환합니다.
+ *
+ * @param degree 변환할 육십분법 각입니다.
+ *
+ * @return 변환된 라디안 각입니다.
+ */
+__forceinline float ToRadian(float degree)
+{
+	return (degree * PI) / 180.0f;
+}
+
+
+/**
+ * @brief 부동 소수점 값이 0에 가까운지 확인합니다.
+ *
+ * @param value 0에 가까운지 확인할 값입니다.
+ * @param epsilon 값 확인을 위한 엡실론 값입니다.
+ *
+ * @return 부동 소수점 값이 0에 가깝다면 true, 그렇지 않다면 false를 반환합니다.
+ */
+__forceinline bool NearZero(float value, float epsilon = EPSILON)
+{
+	return (std::fabsf(value) <= epsilon);
+}
+
+
+/**
+ * @brief 두 값중 큰 값을 반환합니다.
+ *
+ * @param lhs 크기를 비교할 값 중 왼쪽 값입니다.
+ * @param rhs 크기를 비교할 값 중 오른쪽 값입니다.
+ *
+ * @return 두 값 중 큰 값을 반환합니다.
+ */
+template <typename T>
+__forceinline T Max(const T& lhs, const T& rhs)
+{
+	return (lhs < rhs ? rhs : lhs);
+}
+
+
+/**
+ * @brief 두 값중 작은 값을 반환합니다.
+ *
+ * @param lhs 크기를 비교할 값 중 왼쪽 값입니다.
+ * @param rhs 크기를 비교할 값 중 오른쪽 값입니다.
+ *
+ * @return 두 값 중 작은 값을 반환합니다.
+ */
+template <typename T>
+__forceinline T Min(const T& lhs, const T& rhs)
+{
+	return (lhs < rhs ? lhs : rhs);
+}
+
+
+/**
+ * @brief 라디안 각도에 대응하는 사인 값을 얻습니다.
+ *
+ * @param radian 사인 값을 얻을 라디안 각도입니다.
+ *
+ * @return 라디안 각도에 대응하는 사인값을 반환합니다.
+ *
+ * @note
+ * - DirectXMath의 XMScalarSin 참조
+ * - 알고리즘은 11차원 미니맥스(Minimax) 근사치(1th-degree Minimax approximation) 사용
+ * - https://gist.github.com/publik-void/067f7f2fef32dbe5c27d6e215f824c91
+ */
+__forceinline float Sin(float radian)
+{
+	float quotient = ONE_DIV_2PI * radian;
+	if (radian >= 0.0f)
 	{
-		return (radian * 180.0f) / PI;
+		quotient = static_cast<float>(static_cast<int>(quotient + 0.5f));
+	}
+	else
+	{
+		quotient = static_cast<float>(static_cast<int>(quotient - 0.5f));
 	}
 
+	float y = radian - TWO_PI * quotient;
 
-	/**
-	 * @brief 육십분법 각을 라디안 각으로 변환합니다.
-	 *
-	 * @param degree 변환할 육십분법 각입니다.
-	 *
-	 * @return 변환된 라디안 각입니다.
-	 */
-	__forceinline float ToRadian(float degree)
+	if (y > PI_DIV_2)
 	{
-		return (degree * PI) / 180.0f;
+		y = PI - y;
+	}
+	else if (y < -PI_DIV_2)
+	{
+		y = -PI - y;
 	}
 
+	float y2 = y * y;
+	return (((((-2.3889859e-08f * y2 + 2.7525562e-06f) * y2 - 0.00019840874f) * y2 + 0.0083333310f) * y2 - 0.16666667f) * y2 + 1.0f) * y;
+}
 
-	/**
-	 * @brief 부동 소수점 값이 0에 가까운지 확인합니다.
-	 *
-	 * @param value 0에 가까운지 확인할 값입니다.
-	 * @param epsilon 값 확인을 위한 엡실론 값입니다.
-	 *
-	 * @return 부동 소수점 값이 0에 가깝다면 true, 그렇지 않다면 false를 반환합니다.
-	 */
-	__forceinline bool NearZero(float value, float epsilon = EPSILON)
+
+/**
+ * @brief 라디안 각도에 대응하는 코사인 값을 반환합니다.
+ *
+ * @param radian 코사인 값을 얻을 라디안 각도입니다.
+ *
+ * @return 라디안 각도에 대응하는 코사인값을 반환합니다.
+ *
+ * @note
+ * - DirectXMath의 XMScalarCos 참조
+ * - 알고리즘은 10차원 미니맥스(Minimax) 근사치(1th-degree Minimax approximation) 사용
+ * - https://gist.github.com/publik-void/067f7f2fef32dbe5c27d6e215f824c91
+ */
+__forceinline float Cos(float radian)
+{
+	float quotient = ONE_DIV_2PI * radian;
+	if (radian >= 0.0f)
 	{
-		return (std::fabsf(value) <= epsilon);
+		quotient = static_cast<float>(static_cast<int>(quotient + 0.5f));
+	}
+	else
+	{
+		quotient = static_cast<float>(static_cast<int>(quotient - 0.5f));
 	}
 
+	float y = radian - TWO_PI * quotient;
+	float sign = 1.0f;
 
-	/**
-	 * @brief 두 값중 큰 값을 반환합니다.
-	 *
-	 * @param lhs 크기를 비교할 값 중 왼쪽 값입니다.
-	 * @param rhs 크기를 비교할 값 중 오른쪽 값입니다.
-	 *
-	 * @return 두 값 중 큰 값을 반환합니다.
-	 */
-	template <typename T>
-	__forceinline T Max(const T& lhs, const T& rhs)
+	if (y > PI_DIV_2)
 	{
-		return (lhs < rhs ? rhs : lhs);
+		y = PI - y;
+		sign = -1.0f;
+	}
+	else if (y < -PI_DIV_2)
+	{
+		y = -PI - y;
+		sign = -1.0f;
 	}
 
+	float y2 = y * y;
+	float p = ((((-2.6051615e-07f * y2 + 2.4760495e-05f) * y2 - 0.0013888378f) * y2 + 0.041666638f) * y2 - 0.5f) * y2 + 1.0f;
 
-	/**
-	 * @brief 두 값중 작은 값을 반환합니다.
-	 *
-	 * @param lhs 크기를 비교할 값 중 왼쪽 값입니다.
-	 * @param rhs 크기를 비교할 값 중 오른쪽 값입니다.
-	 *
-	 * @return 두 값 중 작은 값을 반환합니다.
-	 */
-	template <typename T>
-	__forceinline T Min(const T& lhs, const T& rhs)
+	return sign * p;
+}
+
+
+/**
+ * @brief 부동 소수점 숫자의 아크 사인을 계산합니다.
+ *
+ * @param value -1.0 에서 1.0 사이의 값입니다.
+ *
+ * @return 입력한 값의 아크 사인(역 사인) 값을 반환합니다.
+ *
+ * @note
+ * - DirectXMath의 XMScalarASin 참조
+ * - 알고리즘은 7차원 미니맥스(Minimax) 근사치(1th-degree Minimax approximation) 사용
+ * - https://gist.github.com/publik-void/067f7f2fef32dbe5c27d6e215f824c91
+ */
+__forceinline float ASin(float value)
+{
+	float x = std::fabsf(value);
+	float omx = 1.0f - x;
+	if (omx < 0.0f)
 	{
-		return (lhs < rhs ? lhs : rhs);
+		omx = 0.0f;
 	}
 
+	float root = sqrtf(omx);
+	float result = ((((((-0.0012624911f * x + 0.0066700901f) * x - 0.0170881256f) * x + 0.0308918810f) * x - 0.0501743046f) * x + 0.0889789874f) * x - 0.2145988016f) * x + 1.5707963050f;
+	result *= root;
 
-	/**
-	 * @brief 라디안 각도에 대응하는 사인 값을 얻습니다.
-	 *
-	 * @param radian 사인 값을 얻을 라디안 각도입니다.
-	 *
-	 * @return 라디안 각도에 대응하는 사인값을 반환합니다.
-	 *
-	 * @note
-	 * - DirectXMath의 XMScalarSin 참조
-	 * - 알고리즘은 11차원 미니맥스(Minimax) 근사치(1th-degree Minimax approximation) 사용
-	 * - https://gist.github.com/publik-void/067f7f2fef32dbe5c27d6e215f824c91
-	 */
-	__forceinline float Sin(float radian)
+	if (value >= 0.0f)
 	{
-		float quotient = ONE_DIV_2PI * radian;
-		if (radian >= 0.0f)
-		{
-			quotient = static_cast<float>(static_cast<int>(quotient + 0.5f));
-		}
-		else
-		{
-			quotient = static_cast<float>(static_cast<int>(quotient - 0.5f));
-		}
+		return (PI_DIV_2 - result);
+	}
+	else
+	{
+		return (result - PI_DIV_2);
+	}
+}
 
-		float y = radian - TWO_PI * quotient;
 
-		if (y > PI_DIV_2)
-		{
-			y = PI - y;
-		}
-		else if (y < -PI_DIV_2)
-		{
-			y = -PI - y;
-		}
-
-		float y2 = y * y;
-		return (((((-2.3889859e-08f * y2 + 2.7525562e-06f) * y2 - 0.00019840874f) * y2 + 0.0083333310f) * y2 - 0.16666667f) * y2 + 1.0f) * y;
+/**
+ * @brief 부동 소수점 숫자의 아크 코사인을 계산합니다.
+ *
+ * @param value -1.0 에서 1.0 사이의 값입니다.
+ *
+ * @return 입력한 값의 코아크 사인(역 사인) 값을 반환합니다.
+ *
+ * @note
+ * - DirectXMath의 XMScalarACos 참조
+ * - 알고리즘은 7차원 미니맥스(Minimax) 근사치(1th-degree Minimax approximation) 사용
+ * - https://gist.github.com/publik-void/067f7f2fef32dbe5c27d6e215f824c91
+ */
+__forceinline float ACos(float value)
+{
+	float x = std::fabsf(value);
+	float omx = 1.0f - x;
+	if (omx < 0.0f)
+	{
+		omx = 0.0f;
 	}
 
+	float root = sqrtf(omx);
+	float result = ((((((-0.0012624911f * x + 0.0066700901f) * x - 0.0170881256f) * x + 0.0308918810f) * x - 0.0501743046f) * x + 0.0889789874f) * x - 0.2145988016f) * x + 1.5707963050f;
+	result *= root;
 
-	/**
-	 * @brief 라디안 각도에 대응하는 코사인 값을 반환합니다.
-	 *
-	 * @param radian 코사인 값을 얻을 라디안 각도입니다.
-	 *
-	 * @return 라디안 각도에 대응하는 코사인값을 반환합니다.
-	 *
-	 * @note
-	 * - DirectXMath의 XMScalarCos 참조
-	 * - 알고리즘은 10차원 미니맥스(Minimax) 근사치(1th-degree Minimax approximation) 사용
-	 * - https://gist.github.com/publik-void/067f7f2fef32dbe5c27d6e215f824c91
-	 */
-	__forceinline float Cos(float radian)
+	if (value >= 0.0f)
 	{
-		float quotient = ONE_DIV_2PI * radian;
-		if (radian >= 0.0f)
-		{
-			quotient = static_cast<float>(static_cast<int>(quotient + 0.5f));
-		}
-		else
-		{
-			quotient = static_cast<float>(static_cast<int>(quotient - 0.5f));
-		}
-
-		float y = radian - TWO_PI * quotient;
-		float sign = 1.0f;
-
-		if (y > PI_DIV_2)
-		{
-			y = PI - y;
-			sign = -1.0f;
-		}
-		else if (y < -PI_DIV_2)
-		{
-			y = -PI - y;
-			sign = -1.0f;
-		}
-
-		float y2 = y * y;
-		float p = ((((-2.6051615e-07f * y2 + 2.4760495e-05f) * y2 - 0.0013888378f) * y2 + 0.041666638f) * y2 - 0.5f) * y2 + 1.0f;
-
-		return sign * p;
+		return result;
 	}
-
-
-	/**
-	 * @brief 부동 소수점 숫자의 아크 사인을 계산합니다.
-	 *
-	 * @param value -1.0 에서 1.0 사이의 값입니다.
-	 *
-	 * @return 입력한 값의 아크 사인(역 사인) 값을 반환합니다.
-	 *
-	 * @note
-	 * - DirectXMath의 XMScalarASin 참조
-	 * - 알고리즘은 7차원 미니맥스(Minimax) 근사치(1th-degree Minimax approximation) 사용
-	 * - https://gist.github.com/publik-void/067f7f2fef32dbe5c27d6e215f824c91
-	 */
-	__forceinline float ASin(float value)
+	else
 	{
-		float x = std::fabsf(value);
-		float omx = 1.0f - x;
-		if (omx < 0.0f)
-		{
-			omx = 0.0f;
-		}
-
-		float root = sqrtf(omx);
-		float result = ((((((-0.0012624911f * x + 0.0066700901f) * x - 0.0170881256f) * x + 0.0308918810f) * x - 0.0501743046f) * x + 0.0889789874f) * x - 0.2145988016f) * x + 1.5707963050f;
-		result *= root;
-
-		if (value >= 0.0f)
-		{
-			return (PI_DIV_2 - result);
-		}
-		else
-		{
-			return (result - PI_DIV_2);
-		}
+		return PI - result;
 	}
+}
 
 
-	/**
-	 * @brief 부동 소수점 숫자의 아크 코사인을 계산합니다.
-	 *
-	 * @param value -1.0 에서 1.0 사이의 값입니다.
-	 *
-	 * @return 입력한 값의 코아크 사인(역 사인) 값을 반환합니다.
-	 *
-	 * @note
-	 * - DirectXMath의 XMScalarACos 참조
-	 * - 알고리즘은 7차원 미니맥스(Minimax) 근사치(1th-degree Minimax approximation) 사용
-	 * - https://gist.github.com/publik-void/067f7f2fef32dbe5c27d6e215f824c91
-	 */
-	__forceinline float ACos(float value)
-	{
-		float x = std::fabsf(value);
-		float omx = 1.0f - x;
-		if (omx < 0.0f)
-		{
-			omx = 0.0f;
-		}
-
-		float root = sqrtf(omx);
-		float result = ((((((-0.0012624911f * x + 0.0066700901f) * x - 0.0170881256f) * x + 0.0308918810f) * x - 0.0501743046f) * x + 0.0889789874f) * x - 0.2145988016f) * x + 1.5707963050f;
-		result *= root;
-
-		if (value >= 0.0f)
-		{
-			return result;
-		}
-		else
-		{
-			return PI - result;
-		}
-	}
+/**
+ * @brief 제곱근을 계산합니다.
+ *
+ * @param x 제곱근을 계산할 부동소수점수입니다.
+ *
+ * @return 제곱근 값을 반환합니다.
+ */
+__forceinline float Sqrt(float x)
+{
+	return std::sqrtf(x);
+}
 
 
-	/**
-	 * @brief 제곱근을 계산합니다.
-	 *
-	 * @param x 제곱근을 계산할 부동소수점수입니다.
-	 *
-	 * @return 제곱근 값을 반환합니다.
-	 */
-	__forceinline float Sqrt(float x)
-	{
-		return std::sqrtf(x);
-	}
+/**
+ * @brief 절댓값을 계산합니다.
+ *
+ * @param x 절댓값을 계산할 부동소수점수입니다.
+ *
+ * @return 절대값을 반환합니다.
+ */
+__forceinline float Abs(float x)
+{
+	return std::fabsf(x);
+}
 
 
-	/**
-	 * @brief 절댓값을 계산합니다.
-	 *
-	 * @param x 절댓값을 계산할 부동소수점수입니다.
-	 *
-	 * @return 절대값을 반환합니다.
-	 */
-	__forceinline float Abs(float x)
-	{
-		return std::fabsf(x);
-	}
+/**
+ * @brief 부동 소수점 나머지를 계산합니다.
+ *
+ * @param x 부동 소수점 나머지를 계산할 왼쪽 피연산자입니다.
+ * @param y 부동 소수점 나머지를 계산할 오른쪽 피연산자입니다.
+ *
+ * @return 계산된 부동 소수점 나머지를 반환합니다.
+ */
+__forceinline float Fmod(float x, float y)
+{
+	return std::fmodf(x, y);
+}
 
 
-	/**
-	 * @brief 부동 소수점 나머지를 계산합니다.
-	 *
-	 * @param x 부동 소수점 나머지를 계산할 왼쪽 피연산자입니다.
-	 * @param y 부동 소수점 나머지를 계산할 오른쪽 피연산자입니다.
-	 *
-	 * @return 계산된 부동 소수점 나머지를 반환합니다.
-	 */
-	__forceinline float Fmod(float x, float y)
-	{
-		return std::fmodf(x, y);
-	}
+/**
+ * @brief 값을 [lower, upper] 범위로 자릅니다.
+ *
+ * @param value 범위로 자를 값입니다.
+ * @param lower 범위의 최소값입니다.
+ * @param upper 범위의 최대값입니다.
+ *
+ * @return 범위로 잘려 나간 값을 반환합니다.
+ */
+template <typename T>
+__forceinline T Clamp(const T& value, const T& lower, const T& upper)
+{
+	return Min<T>(upper, Max<T>(lower, value));
+}
 
 
-	/**
-	 * @brief 값을 [lower, upper] 범위로 자릅니다.
-	 *
-	 * @param value 범위로 자를 값입니다.
-	 * @param lower 범위의 최소값입니다.
-	 * @param upper 범위의 최대값입니다.
-	 *
-	 * @return 범위로 잘려 나간 값을 반환합니다.
-	 */
-	template <typename T>
-	__forceinline T Clamp(const T& value, const T& lower, const T& upper)
-	{
-		return Min<T>(upper, Max<T>(lower, value));
-	}
+/**
+ * @brief 임의의 정수를 생성합니다.
+ *
+ * @param minValue 생성할 난수 범위의 최솟값입니다.
+ * @param maxValue 생성할 난수 범위의 최댓값입니다.
+ *
+ * @return 생성된 임의의 정수를 반환합니다.
+ */
+__forceinline int32_t GenerateRandomInt(int32_t minValue, int32_t maxValue)
+{
+	std::random_device randomDevice;
+	std::mt19937 generator(randomDevice());
+	std::uniform_int_distribution<int32_t> distribution(Min<int32_t>(minValue, maxValue), Max<int32_t>(minValue, maxValue));
+
+	return distribution(generator);
+}
 
 
-	/**
-	 * @brief 임의의 정수를 생성합니다.
-	 *
-	 * @param minValue 생성할 난수 범위의 최솟값입니다.
-	 * @param maxValue 생성할 난수 범위의 최댓값입니다.
-	 *
-	 * @return 생성된 임의의 정수를 반환합니다.
-	 */
-	__forceinline int32_t GenerateRandomInt(int32_t minValue, int32_t maxValue)
-	{
-		std::random_device randomDevice;
-		std::mt19937 generator(randomDevice());
-		std::uniform_int_distribution<int32_t> distribution(Min<int32_t>(minValue, maxValue), Max<int32_t>(minValue, maxValue));
+/**
+ * @brief 임의의 실수를 생성합니다.
+ *
+ * @param minValue 생성할 난수 범위의 최솟값입니다.
+ * @param maxValue 생성할 난수 범위의 최댓값입니다.
+ *
+ * @return 생성된 임의의 실수를 반환합니다.
+ */
+__forceinline float GenerateRandomFloat(float minValue, float maxValue)
+{
+	std::random_device randomDevice;
+	std::mt19937 generator(randomDevice());
+	std::uniform_real_distribution<float> distribution(Min<float>(minValue, maxValue), Max<float>(minValue, maxValue));
 
-		return distribution(generator);
-	}
-
-
-	/**
-	 * @brief 임의의 실수를 생성합니다.
-	 *
-	 * @param minValue 생성할 난수 범위의 최솟값입니다.
-	 * @param maxValue 생성할 난수 범위의 최댓값입니다.
-	 *
-	 * @return 생성된 임의의 실수를 반환합니다.
-	 */
-	__forceinline float GenerateRandomFloat(float minValue, float maxValue)
-	{
-		std::random_device randomDevice;
-		std::mt19937 generator(randomDevice());
-		std::uniform_real_distribution<float> distribution(Min<float>(minValue, maxValue), Max<float>(minValue, maxValue));
-
-		return distribution(generator);
-	}
+	return distribution(generator);
+}
 
 
 /**
