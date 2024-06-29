c8f8b0bab6568bc239999fc22b55aa4cc5b67a8d
choijione <choijione02@gmail.com>
2024-06-29 11:01:45 +0900

    엔진 기능 구현 테스트 코드 추가

--- CoinDash3D/Src/Main.cpp
+++ CoinDash3D/Src/Main.cpp
@@ -7,33 +7,33 @@
 
 #include <SDL2/SDL.h>
 
+#include "GameEngine.h"
+#include "GameInput.h"
+#include "GameTimer.h"
+#include "GameMath.h"
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	SDL_Init(SDL_INIT_EVERYTHING);
-	
-	SDL_Window* window = nullptr;
-	window = SDL_CreateWindow("CoinDash3D", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::MOVED, [&]() { SDL_Log("MOVED"); });
+	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::FOCUS_LOST, [&]() { SDL_Log("FOCUS_LOST"); });
+	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::LEAVE, [&]() { SDL_Log("LEAVE"); });
+	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::RESIZED, [&]() { SDL_Log("RESIZED"); });
+	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::SIZE_CHANGED, [&]() { SDL_Log("SIZE_CHANGED"); });
+	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::CLOSE, [&]() { 
+		SDL_Log("CLOSE"); 
+		});
 
-	SDL_Event e;
-	bool quit = false;
-	while (!quit)
-	{
-		while (SDL_PollEvent(&e))
+	GameMaker::GameEngine::Init();
+	GameMaker::GameEngine::RunLoop(
+		[&](float deltaSeconds) 
 		{
-			if (e.type == SDL_QUIT)
-			{
-				quit = true;
-			}
 		}
-	}
+	);
+	GameMaker::GameEngine::Shutdown();
 
-	SDL_DestroyWindow(window);
-	window = nullptr;
-
-	SDL_Quit();
 	return 0;
 }
