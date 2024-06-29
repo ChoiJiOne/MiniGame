647f61b3425172ca25f6b83d05d7332980aeb541
choijione <choijione02@gmail.com>
2024-06-29 16:46:16 +0900

    GameMaker 엔진의 기능 구현 테스트 코드 추가

--- CoinDash3D/Src/Main.cpp
+++ CoinDash3D/Src/Main.cpp
@@ -8,9 +8,11 @@
 #include <SDL2/SDL.h>
 
 #include "GameEngine.h"
-#include "GameInput.h"
 #include "GameTimer.h"
 #include "GameMath.h"
+#include "InputManager.h"
+#include "RenderManager.h"
+#include "FileUtils.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -18,19 +20,26 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::MOVED, [&]() { SDL_Log("MOVED"); });
-	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::FOCUS_LOST, [&]() { SDL_Log("FOCUS_LOST"); });
-	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::LEAVE, [&]() { SDL_Log("LEAVE"); });
-	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::RESIZED, [&]() { SDL_Log("RESIZED"); });
-	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::SIZE_CHANGED, [&]() { SDL_Log("SIZE_CHANGED"); });
-	GameMaker::GameInput::AddWindowEventAction(GameMaker::EWindowEvent::CLOSE, [&]() { 
+	GameMaker::InputManager& manager = GameMaker::InputManager::Get();
+
+	manager.AddWindowEventAction(GameMaker::EWindowEvent::MOVED, [&]() { SDL_Log("MOVED"); });
+	manager.AddWindowEventAction(GameMaker::EWindowEvent::FOCUS_LOST, [&]() { SDL_Log("FOCUS_LOST"); });
+	manager.AddWindowEventAction(GameMaker::EWindowEvent::LEAVE, [&]() { SDL_Log("LEAVE"); });
+	manager.AddWindowEventAction(GameMaker::EWindowEvent::RESIZED, [&]() { SDL_Log("RESIZED"); });
+	manager.AddWindowEventAction(GameMaker::EWindowEvent::SIZE_CHANGED, [&]() { SDL_Log("SIZE_CHANGED"); });
+	manager.AddWindowEventAction(GameMaker::EWindowEvent::CLOSE, [&]() { 
 		SDL_Log("CLOSE"); 
 		});
+
+	GameMaker::FileUtils::MakeDirectory("Test");
 
 	GameMaker::GameEngine::Init();
 	GameMaker::GameEngine::RunLoop(
 		[&](float deltaSeconds) 
 		{
+			GameMaker::RenderManager::Get().BeginFrame(1.0f, 0.0f, 0.0f, 1.0f);
+			GameMaker::RenderManager::Get().EndFrame();
+
 		}
 	);
 	GameMaker::GameEngine::Shutdown();
