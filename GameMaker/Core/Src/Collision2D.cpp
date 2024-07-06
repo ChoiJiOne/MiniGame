44d0771ef18e3e87050324487bbb33b6b33c3f3a
choijione <choijione02@gmail.com>
2024-07-06 13:23:54 +0900

    2D 점과 점 끼리의 충돌 처리 기능 추가

--- GameMaker/Core/Src/Collision2D.cpp
+++ GameMaker/Core/Src/Collision2D.cpp
@@ -5,9 +5,46 @@ using namespace GameMaker;
 
 bool Point2D::Intersect(const ICollision2D* target) const
 {
-	return false;
-}
+	CHECK(target != nullptr);
 
+	EType type = target->GetType();
+	bool bIsIntersect = false;
+
+	switch (type)
+	{
+	case ICollision2D::EType::POINT:
+	{
+		const Point2D* other = reinterpret_cast<const Point2D*>(target);
+		bIsIntersect = (center == other->center);
+		break;
+	}
+
+	case ICollision2D::EType::LINE:
+	{
+		break;
+	}
+
+	case ICollision2D::EType::CIRCLE: 
+	{
+		break;
+	}
+
+	case ICollision2D::EType::RECT:
+	{
+		break;
+	}
+
+	case ICollision2D::EType::ORIENTED_RECT:
+	{
+		break;
+	}
+	break;
+
+	default: break;// ICollision2D::EType::NONE:
+	}
+
+	return bIsIntersect;
+}
 
 bool Line2D::Intersect(const ICollision2D* target) const
 {
