97285bd8388db26470e898d19b146c016f812817
choijione <choijione02@gmail.com>
2024-07-10 18:21:49 +0900

    프로젝트 이름을 인자로 받아 파이썬 스크립트를 실행하는 기능 추가

--- Setup.bat
+++ Setup.bat
@@ -9,8 +9,25 @@ IF %ERRORLEVEL% EQU 0 (
     ECHO Python is installed...
 ) ELSE (
     ECHO Python is not installed...
+    ECHO ====================
+    ECHO Done Setup Project
+    ECHO ====================
     GOTO:EOF
 )
+
+SET PROJECT_NAME=%1
+if "%PROJECT_NAME%" == "" (
+    ECHO Project name is empty...
+    ECHO ====================
+    ECHO Done Setup Project
+    ECHO ====================
+    GOTO:EOF
+) else (
+    ECHO Project name is "%PROJECT_NAME%"...
+)
+
+SET SETUP_SCRIPT_PATH=%~dp0GameMaker\Script\Setup.py
+python "%SETUP_SCRIPT_PATH%" "%PROJECT_NAME%"
 
 ECHO ====================
 ECHO Done Setup Project
