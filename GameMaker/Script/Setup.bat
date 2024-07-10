51bdbb15907ea37f3054492e4cbbeef91e29099b
choijione <choijione02@gmail.com>
2024-07-10 18:10:09 +0900

    세팅할 환경에 Python이 설치되어 있는지 확인하는 기능 추가

---
+++ GameMaker/Script/Setup.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+ECHO ====================
+ECHO Start Setup Project
+ECHO ====================
+
+python --version >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Python is installed...
+) ELSE (
+    ECHO Python is not installed...
+    GOTO:EOF
+)
+
+ECHO ====================
+ECHO Done Setup Project
+ECHO ====================
