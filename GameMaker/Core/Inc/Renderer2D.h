1c33e82794beb3a4d4509d02bdd9083977b89b1a
choijione <choijione02@gmail.com>
2024-07-20 15:51:31 +0900

    렌더링 명령을 수행할 때 참조할 커맨드 구조체 추가

--- GameMaker/Core/Inc/Renderer2D.h
+++ GameMaker/Core/Inc/Renderer2D.h
@@ -36,25 +36,34 @@ public:
 	void Begin(const Camera2D* camera2D = nullptr);
 	void End();
 
+	// EDrawMode::TRIANGLES
 	void DrawPoint(const Vec2f* positions, uint32_t size, const Vec4f& color, float pointSize = 1.0f);
 	void DrawPoint(const Vec2f* positions, const Vec4f* colors, uint32_t size, float pointSize = 1.0f);
+	void DrawTriangle(const Vec2f& fromPos, const Vec2f& byPos, const Vec2f& toPos, const Vec4f& color);
+	void DrawTriangle(const Vec2f& fromPos, const Vec4f& fromColor, const Vec2f& byPos, const Vec4f& byColor, const Vec2f& toPos, const Vec4f& toColor);
+	void DrawRect(const Vec2f& center, float w, float h, const Vec4f& color, float rotate = 0.0f);
+	void DrawRoundRect(const Vec2f& center, float w, float h, float side, const Vec4f& color, float rotate = 0.0f);
+	void DrawCircle(const Vec2f& center, float radius, const Vec4f& color, int32_t sliceCount = 300);
+	void DrawCircleWireframe(const Vec2f& center, float radius, const Vec4f& color, int32_t sliceCount = 300);
+	void DrawEllipse(const Vec2f& center, float xAxis, float yAxis, const Vec4f& color, float rotate = 0.0f, int32_t sliceCount = 300);
+	void DrawEllipseWireframe(const Vec2f& center, float xAxis, float yAxis, const Vec4f& color, float rotate = 0.0f, int32_t sliceCount = 300);
+
+
+	// EDrawMode::LINE_STRIP or EDrawMode::LINES
 	void DrawLine(const Vec2f* positions, const Vec4f* colors, uint32_t size);
 	void DrawLine(const Vec2f& startPos, const Vec2f& endPos, const Vec4f& color);
 	void DrawLine(const Vec2f& startPos, const Vec4f& startColor, const Vec2f& endPos, const Vec4f& endColor);
 	void DrawLines(const Vec2f* positions, uint32_t size, const Vec4f& color);
 	void DrawLines(const Vec2f* positions, const Vec4f* colors, uint32_t size);
-	void DrawTriangle(const Vec2f& fromPos, const Vec2f& byPos, const Vec2f& toPos, const Vec4f& color);
-	void DrawTriangle(const Vec2f& fromPos, const Vec4f& fromColor, const Vec2f& byPos, const Vec4f& byColor, const Vec2f& toPos, const Vec4f& toColor);
 	void DrawTriangleWireframe(const Vec2f& fromPos, const Vec2f& byPos, const Vec2f& toPos, const Vec4f& color);
 	void DrawTriangleWireframe(const Vec2f& fromPos, const Vec4f& fromColor, const Vec2f& byPos, const Vec4f& byColor, const Vec2f& toPos, const Vec4f& toColor);
-	void DrawRect(const Vec2f& center, float w, float h, const Vec4f& color, float rotate = 0.0f);
 	void DrawRectWireframe(const Vec2f& center, float w, float h, const Vec4f& color, float rotate = 0.0f);
-	void DrawRoundRect(const Vec2f& center, float w, float h, float side, const Vec4f& color, float rotate = 0.0f);
 	void DrawRoundRectWireframe(const Vec2f& center, float w, float h, float side, const Vec4f& color, float rotate = 0.0f);
-	void DrawCircle(const Vec2f& center, float radius, const Vec4f& color, int32_t sliceCount = 300);
 	void DrawCircleWireframe(const Vec2f& center, float radius, const Vec4f& color, int32_t sliceCount = 300);
-	void DrawEllipse(const Vec2f& center, float xAxis, float yAxis, const Vec4f& color, float rotate = 0.0f, int32_t sliceCount = 300);
 	void DrawEllipseWireframe(const Vec2f& center, float xAxis, float yAxis, const Vec4f& color, float rotate = 0.0f, int32_t sliceCount = 300);
+
+
+
 	void DrawString(const TTFont* font, const std::wstring& text, const Vec2f& pos, const Vec4f& color);
 	void DrawSprite(const ITexture* texture, const Vec2f& center, float w, float h, float rotate = 0.0f, bool bFlipH = false, bool bFlipV = false);
 
@@ -103,25 +112,35 @@ private:
 		Vec4f color;
 	};
 
-	enum class EMode
+	enum class EType
 	{
 		GEOMETRY = 0x00,
 		STRING   = 0x01,
 		SPRITE   = 0x02,
 	};
 
+	struct RenderCommand
+	{
+		EDrawMode drawMode;
+		uint32_t startVertexIndex;
+		uint32_t vertexCount;
+		EType type;
+		ITexture* texture;
+		TTFont* font;
+	};
+
 
 private:
 	/** 텍스트 영역 설정 시 좌표는 왼쪽 상단 좌표 기준입니다. */
-	uint32_t SetGlyphVertexBuffer(const TTFont* font, const std::wstring& text, const Vec2f& pos, const Vec4f& color);
-	void Draw(const Mat4x4& transform, const EDrawMode& drawMode, uint32_t vertexCount, const EMode& mode);
+	//uint32_t SetGlyphVertexBuffer(const TTFont* font, const std::wstring& text, const Vec2f& pos, const Vec4f& color);
+	//void Draw(const Mat4x4& transform, const EDrawMode& drawMode, uint32_t vertexCount, const EMode& mode);
 
 private:
-	static const int32_t MAX_VERTEX_SIZE = 10000;
+	static const int32_t MAX_VERTEX_SIZE = 20000;
 	static const int32_t MAX_SLICE_SIZE = 20;
 	static const int32_t GLYPH_ATLAS_BIND_SLOT = 0;
 	static const int32_t SPRITE_BIND_SLOT = 1;
-
+	
 	std::array<Vertex, MAX_VERTEX_SIZE> vertices_;
 	bool bIsBegin_ = false;
 	float pointSize_ = 1.0f;
@@ -131,6 +150,12 @@ private:
 	Shader* shader_ = nullptr;
 	bool bIsBeforeEnableDepth_ = true;
 	bool bIsBeforeEnableCull_ = false;
+
+
+	static const int32_t MAX_RENDER_COMMAND = 2000;
+	int32_t commandQueueHead_ = -1;
+	int32_t commandQueueTail_ = -1;
+	std::array<RenderCommand, MAX_RENDER_COMMAND> renderCommandQueue_;
 };
 
 }
