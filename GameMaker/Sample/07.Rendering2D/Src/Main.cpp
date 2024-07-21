2eb64b79a1170de31a7a049e8d8d8192a0bcf8b6
choijione <choijione02@gmail.com>
2024-07-21 21:29:03 +0900

    2D 점 및 선을 렌더링하는 테스트 코드 추가

--- GameMaker/Sample/07.Rendering2D/Src/Main.cpp
+++ GameMaker/Sample/07.Rendering2D/Src/Main.cpp
@@ -5,6 +5,8 @@
 #include <crtdbg.h>
 #endif
 
+#include <imgui.h>
+
 #include "GameEngine.h"
 #include "Renderer2D.h"
 #include "RenderManager.h"
@@ -17,6 +19,7 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	
 	GameMaker::GameEngine::Init({ "07.Rendering2D", 100, 100, 800, 600, false, false });
 
+	GameMaker::RenderManager::Get().SetVsyncMode(false);
 	GameMaker::Renderer2D* renderer = GameMaker::RenderManager::Get().GetRenderer2D();
 
 	GameMaker::Vec2f center(0.0f, 0.0f);
@@ -24,6 +27,11 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	GameMaker::GameEngine::RunLoop(
 		[&](float deltaSeconds)
 		{
+			ImGui::Begin("Framerate", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
+			ImGui::SetWindowPos(ImVec2(0.0f, 0.0f));
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			ImGui::End();
+
 			GameMaker::RenderManager::Get().BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
 			renderer->Begin();
 			{
@@ -38,12 +46,44 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 				//renderer->DrawRect(GameMaker::Vec2f(0.0f, -200.0f), 200.0f, 50.0f, GameMaker::Vec4f(1.0f, 0.0f, 1.0f, 1.0f));
 				//renderer->DrawRect(GameMaker::Vec2f(0.0f, -100.0f), 200.0f, 50.0f, GameMaker::Vec4f(1.0f, 1.0f, 1.0f, 1.0f));
 
-				static float time = 0.0f;
-				time += deltaSeconds;
+				//static float time = 0.0f;
+				//time += deltaSeconds;
 
-				renderer->DrawRect(GameMaker::Vec2f(0.0f, 100.0f), 200.0f, 100.0f, GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f), time);
-				renderer->DrawRect(GameMaker::Vec2f(0.0f, 50.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f));
-				renderer->DrawRect(GameMaker::Vec2f(0.0f, 0.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f), -time / 10.0f);
+				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 100.0f), 200.0f, 100.0f, GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f), time);
+				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 50.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f));
+				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 0.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f), -time / 10.0f);
+				//GameMaker::Vec2f point;
+
+				//renderer->DrawPoint(GameMaker::Vec2f(), GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f), 50.0f);
+
+
+				for (float x = -400.0f; x <= 400.0f; x += 10.0f)
+				{
+					GameMaker::Vec4f color = (x == 0.0f) ? GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f) : GameMaker::Vec4f(0.5f, 0.5f, 0.5f, 0.5f);
+					renderer->DrawLine(GameMaker::Vec2f(x, -300.0f), GameMaker::Vec2f(x, +300.0f), color);
+				}
+				
+				for (float y = -300.0f; y <= 300.0f; y += 10.0f)
+				{
+					GameMaker::Vec4f color = (y == 0.0f) ? GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f) : GameMaker::Vec4f(0.5f, 0.5f, 0.5f, 0.5f);
+					renderer->DrawLine(GameMaker::Vec2f(-400.0f, y), GameMaker::Vec2f(+400.0f, y), color);
+				}
+
+				//static float time = 0.0f;
+				//time += deltaSeconds;
+				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 100.0f), 200.0f, 100.0f, GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f), time);
+				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 50.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f));
+				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 0.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f), -time / 10.0f);
+
+				renderer->DrawLine(
+					GameMaker::Vec2f(0.0f, 0.0f), GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f),
+					GameMaker::Vec2f(101.7f, 97.3f), GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f)
+				);
+				renderer->DrawLine(
+					GameMaker::Vec2f(101.7f, 97.3f), GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f),
+					GameMaker::Vec2f(203.3f, 57.3f), GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f)
+				);
+
 			}
 			renderer->End();
 			GameMaker::RenderManager::Get().EndFrame();
