773d6854a0c85657343b987b8bcea926a8b8f0f4
choijione <choijione02@gmail.com>
2024-07-22 10:20:37 +0900

    2D 렌더링을 테스트하기 위한 코드 추가

--- GameMaker/Sample/07.Rendering2D/Src/Main.cpp
+++ GameMaker/Sample/07.Rendering2D/Src/Main.cpp
@@ -69,20 +69,58 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 					renderer->DrawLine(GameMaker::Vec2f(-400.0f, y), GameMaker::Vec2f(+400.0f, y), color);
 				}
 
-				//static float time = 0.0f;
-				//time += deltaSeconds;
-				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 100.0f), 200.0f, 100.0f, GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f), time);
-				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 50.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f));
-				//renderer->DrawRect(GameMaker::Vec2f(0.0f, 0.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f), -time / 10.0f);
+				static float time = 0.0f;
+				time += deltaSeconds;
+				float rate = time / 10.0f;
+				rate = GameMaker::Clamp<float>(rate, 0.0f, 1.0f);
+				//renderer->DrawRectWireframe(GameMaker::Vec2f(0.0f, 100.0f), 200.0f, 100.0f, GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f), time);
+				//renderer->DrawRectWireframe(GameMaker::Vec2f(0.0f, 50.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f));
+				//renderer->DrawRectWireframe(GameMaker::Vec2f(0.0f, 0.0f), 200.0f, 100.0f, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f), -time / 10.0f);
 
-				renderer->DrawLine(
-					GameMaker::Vec2f(0.0f, 0.0f), GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f),
-					GameMaker::Vec2f(101.7f, 97.3f), GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f)
-				);
-				renderer->DrawLine(
-					GameMaker::Vec2f(101.7f, 97.3f), GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f),
-					GameMaker::Vec2f(203.3f, 57.3f), GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f)
-				);
+				renderer->DrawRoundRect(GameMaker::Vec2f(0.0f, 0.0f), 200.0f, 100.0f, 20.0f, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f));
+
+				//GameMaker::Vec2f start(-100.0f, 0.0f);
+				//GameMaker::Vec2f control(0.0f, 50.0f);
+				//GameMaker::Vec2f end(+100.0f, 0.0f);
+
+
+
+				//renderer->DrawPoint(start, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f), 5.0f);
+				//renderer->DrawPoint(end, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f), 5.0f);
+				//renderer->DrawPoint(control, GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f), 5.0f);
+
+				//renderer->DrawPoint(GameMaker::Vec2f::Bezier(start, end, control, rate), GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f), 5.0f);
+
+				//renderer->DrawTriangleWireframe(GameMaker::Vec2f(-100.5f, -150.75f), GameMaker::Vec2f(0.0f, 299.9f), GameMaker::Vec2f(200.25f, -100.5f), GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f));
+				//renderer->DrawTriangleWireframe(GameMaker::Vec2f(-300.33f, 250.67f), GameMaker::Vec2f(-200.2f, -250.5f), GameMaker::Vec2f(100.5f, -100.25f), GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f));
+				//renderer->DrawTriangleWireframe(GameMaker::Vec2f(150.5f, -200.75f), GameMaker::Vec2f(250.75f, 100.5f), GameMaker::Vec2f(-50.3f, 200.8f), GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f));
+
+				//renderer->DrawTriangleWireframe(
+				//	GameMaker::Vec2f(-100.5f, -150.75f), GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f),
+				//	GameMaker::Vec2f(0.0f, 299.9f), GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f),
+				//	GameMaker::Vec2f(200.25f, -100.5f), GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f)
+				//);
+
+				//renderer->DrawTriangleWireframe(
+				//	GameMaker::Vec2f(-300.33f, 250.67f), GameMaker::Vec4f(1.0f, 1.0f, 0.0f, 1.0f),
+				//	GameMaker::Vec2f(-200.2f, -250.5f), GameMaker::Vec4f(1.0f, 0.0f, 1.0f, 1.0f),
+				//	GameMaker::Vec2f(100.5f, -100.25f), GameMaker::Vec4f(0.0f, 1.0f, 1.0f, 1.0f)
+				//);
+
+				//renderer->DrawTriangleWireframe(
+				//	GameMaker::Vec2f(150.5f, -200.75f), GameMaker::Vec4f(0.5f, 0.5f, 0.5f, 1.0f),
+				//	GameMaker::Vec2f(250.75f, 100.5f), GameMaker::Vec4f(0.25f, 0.25f, 0.25f, 1.0f),
+				//	GameMaker::Vec2f(-50.3f, 200.8f), GameMaker::Vec4f(0.75f, 0.75f, 0.75f, 1.0f)
+				//);
+
+				//renderer->DrawLine(
+				//	GameMaker::Vec2f(0.0f, 0.0f), GameMaker::Vec4f(1.0f, 0.0f, 0.0f, 1.0f),
+				//	GameMaker::Vec2f(101.7f, 97.3f), GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f)
+				//);
+				//renderer->DrawLine(
+				//	GameMaker::Vec2f(101.7f, 97.3f), GameMaker::Vec4f(0.0f, 0.0f, 1.0f, 1.0f),
+				//	GameMaker::Vec2f(203.3f, 57.3f), GameMaker::Vec4f(0.0f, 1.0f, 0.0f, 1.0f)
+				//);
 
 			}
 			renderer->End();
