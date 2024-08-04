b7e46f2301de2f2ea905eff53959884c54a3f394
choijione <choijione02@gmail.com>
2024-08-04 11:50:07 +0900

    Texture2D에 KTX 관련 기능 삭제

--- GameMaker/Core/Src/Texture2D.cpp
+++ GameMaker/Core/Src/Texture2D.cpp
@@ -2,7 +2,6 @@
 #pragma warning(disable: 26451)
 
 #include <glad/glad.h>
-#include <ktx.h>
 #include <stb_image.h>
 
 #include "Assertion.h"
@@ -20,62 +19,45 @@ Texture2D::Texture2D(const std::string& path, bool bIsVerticallyFlip)
 {
 	std::string extension = FileUtils::GetFileExtension(path);
 
-	if (extension == "ktx") /** 반드시 엔진에 내장된 ktx.exe를 사용해서 변환된 텍스처만 사용! */
+	int32_t width;
+	int32_t height;
+	int32_t channels;
+	std::vector<uint8_t> buffer;
+	ReadPixelBufferFromFile(path, width, height, channels, buffer, bIsVerticallyFlip);
+
+	GLenum format = 0xFFFF;
+	switch (channels)
 	{
-		ktxTexture* ktxTexturePtr = nullptr;
-		CHECK(ktxTexture_CreateFromNamedFile(path.c_str(), KTX_TEXTURE_CREATE_NO_FLAGS, &ktxTexturePtr) == KTX_SUCCESS);
+	case IMAGE_FORMAT_R:
+		format = GL_RED;
+		break;
 
-		GL_FAILED(glGenTextures(1, &textureID_));
+	case IMAGE_FORMAT_RG:
+		format = GL_RG;
+		break;
 
-		GLenum target;
-		GLenum glerror;
-		CHECK(ktxTexture_GLUpload(ktxTexturePtr, &textureID_, &target, &glerror) == KTX_SUCCESS);
+	case IMAGE_FORMAT_RGB:
+		format = GL_RGB;
+		break;
 
-		ktxTexture_Destroy(ktxTexturePtr);
-		ktxTexturePtr = nullptr;
+	case IMAGE_FORMAT_RGBA:
+		format = GL_RGBA;
+		break;
+
+	default:
+		ASSERT(false, "undefined texture resource format");
+		break;
 	}
-	else
-	{
-		int32_t width;
-		int32_t height;
-		int32_t channels;
-		std::vector<uint8_t> buffer;
-		ReadPixelBufferFromFile(path, width, height, channels, buffer, bIsVerticallyFlip);
 
-		GLenum format = 0xFFFF;
-		switch (channels)
-		{
-		case IMAGE_FORMAT_R:
-			format = GL_RED;
-			break;
-
-		case IMAGE_FORMAT_RG:
-			format = GL_RG;
-			break;
-
-		case IMAGE_FORMAT_RGB:
-			format = GL_RGB;
-			break;
-
-		case IMAGE_FORMAT_RGBA:
-			format = GL_RGBA;
-			break;
-
-		default:
-			ASSERT(false, "undefined texture resource format");
-			break;
-		}
-
-		GL_FAILED(glGenTextures(1, &textureID_));
-		GL_FAILED(glBindTexture(GL_TEXTURE_2D, textureID_));
-		GL_FAILED(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
-		GL_FAILED(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
-		GL_FAILED(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
-		GL_FAILED(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
-		GL_FAILED(glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, buffer.data()));
-		GL_FAILED(glGenerateMipmap(GL_TEXTURE_2D));
-		GL_FAILED(glBindTexture(GL_TEXTURE_2D, 0));
-	}
+	GL_FAILED(glGenTextures(1, &textureID_));
+	GL_FAILED(glBindTexture(GL_TEXTURE_2D, textureID_));
+	GL_FAILED(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE));
+	GL_FAILED(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE));
+	GL_FAILED(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR));
+	GL_FAILED(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+	GL_FAILED(glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, buffer.data()));
+	GL_FAILED(glGenerateMipmap(GL_TEXTURE_2D));
+	GL_FAILED(glBindTexture(GL_TEXTURE_2D, 0));
 
 	bIsInitialized_ = true;
 }
