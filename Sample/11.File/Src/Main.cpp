86958e5879e7ebff6951f72bf9efc1fe70433885
choijione <choijione02@gmail.com>
2024-08-23 16:30:45 +0900

    작업 경로를 출력하는 기능 추가

--- Sample/11.File/Src/Main.cpp
+++ Sample/11.File/Src/Main.cpp
@@ -1,4 +1,16 @@
+//#include 
+
+#include <iostream>
+
+#include "FileManager.h"
+
 int main(void)
 {
+	GameMaker::FileManager::Get().Startup();
+	{
+		std::cout << "WORKING DIRECTORY(ANSI): " << GameMaker::FileManager::Get().GetWorkingDirectoryA() << std::endl;
+		std::wcout << L"WORKING DIRECTORY(UNICODE): " << GameMaker::FileManager::Get().GetWorkingDirectoryW() << std::endl;
+	}
+	GameMaker::FileManager::Get().Shutdown();
 	return 0;
 }
