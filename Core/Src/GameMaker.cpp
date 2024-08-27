084d51a4cc03a93ee6a2eced872701c517397303
choijione <choijione02@gmail.com>
2024-08-28 08:44:25 +0900

    GameMaker 엔진의 초기화 여부를 확인하는 변수 추가

--- Core/Src/GameMaker.cpp
+++ Core/Src/GameMaker.cpp
@@ -1,4 +1,4 @@
-/** 서드 파티 라이브러리 */
+/** 서드 파티 라이브러리 사용을 위한 헤더 선언 시작 */
 #include <imgui.h>
 #include <imgui_impl_sdl2.h>
 #include <imgui_impl_opengl3.h>
@@ -6,21 +6,32 @@
 #include <SDL2/SDL.h>
 
 #include <mimalloc-new-delete.h>
-/** 서드 파티 라이브러리 */
+/** 서드 파티 라이브러리 사용을 위한 헤더 선언 종료*/
 
 #include "GameMaker.h"
 
+bool bIsStartup = false;
+
 GameError GameMaker::Startup()
 {
-	return GameError(ErrorCode::OK, "");
+	if (bIsStartup)
+	{
+		return GameError(ErrorCode::FAILED_STARTUP, "Already startup GameMaker.");
+	}
+
+	return GameError(ErrorCode::OK, "Succeed startup GameMaker.");
 }
 
 GameError GameMaker::Shutdown()
 {
-	return GameError(ErrorCode::OK, "");
+	if (!bIsStartup)
+	{
+		return GameError(ErrorCode::FAILED_SHUTDOWN, "Startup has not called, or Shutdown has already been invoked.");
+	}
+
+	return GameError(ErrorCode::OK, "Succeed shutdown GameMaker.");
 }
 
-GameError GameMaker::RunLoop(const std::function<void(float)>& callback)
+void GameMaker::RunLoop(const std::function<void(float)>& callback)
 {
-	return GameError(ErrorCode::OK, "");
 }
