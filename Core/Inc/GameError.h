862ff8462b701d9b645a9723abc5ab9be9096ff7
choijione <choijione02@gmail.com>
2024-08-28 08:24:29 +0900

    게임의 에러 코드 및 메시지를 나타내는 객체 추가

---
+++ Core/Inc/GameError.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <string>
+
+
+enum class ErrorCode : int32_t
+{
+	OK     = 0x00,
+	FAILED = 0x01,
+};
+
+class GameError
+{
+public:
+	GameError() {}
+	GameError(const ErrorCode& errorCode, const std::string message) : errorCode_(errorCode), message_(message) {}
+	GameError(GameError&& instance) noexcept : errorCode_(instance.errorCode_), message_(instance.message_) {}
+	GameError(const GameError& instance) noexcept : errorCode_(instance.errorCode_), message_(instance.message_) {}
+
+	virtual ~GameError() {}
+
+	GameError& operator=(GameError&& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		errorCode_ = instance.errorCode_;
+		message_ = instance.message_;
+
+		return *this;
+	}
+
+	GameError& operator=(const GameError& instance) noexcept
+	{
+		if (this == &instance) return *this;
+
+		errorCode_ = instance.errorCode_;
+		message_ = instance.message_;
+
+		return *this;
+	}
+
+	const ErrorCode& GetCode() const { return errorCode_; }
+	const std::string& GetMessage() const { return message_; }
+	
+private:
+	ErrorCode errorCode_ = ErrorCode::OK;
+	std::string message_;
+};
