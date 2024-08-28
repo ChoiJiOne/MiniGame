c5fa67317d2881e0c770c9a83a40b6a8adfad42c
choijione <choijione02@gmail.com>
2024-08-28 11:55:22 +0900

    네임 스페이스의 들여 쓰기 수정

--- Core/Inc/GameMaker.h
+++ Core/Inc/GameMaker.h
@@ -7,12 +7,14 @@
 
 namespace GameMaker
 {
-	/**  윈도우의 크기 변경과 풀 스크린 여부의 기본 값은 false입니다.  */
-	GameError Startup(const char* title, int32_t x, int32_t y, int32_t w, int32_t h, bool bIsResizble = false, bool bIsFullscreen = false);
-	GameError Shutdown();
 
-	void RunLoop(const std::function<void(float)>& frameCallback);
+/**  윈도우의 크기 변경과 풀 스크린 여부의 기본 값은 false입니다.  */
+GameError Startup(const char* title, int32_t x, int32_t y, int32_t w, int32_t h, bool bIsResizble = false, bool bIsFullscreen = false);
+GameError Shutdown();
 
-	int32_t GetNumVideoDisplay();
-	GameError GetVideoDisplaySize(int32_t index, GameMath::Vec2i& outSize);
+void RunLoop(const std::function<void(float)>& frameCallback);
+
+int32_t GetNumVideoDisplay();
+GameError GetVideoDisplaySize(int32_t index, GameMath::Vec2i& outSize);
+
 }
--- Core/Inc/GameUtils.h
+++ Core/Inc/GameUtils.h
@@ -8,35 +8,37 @@
 
 namespace GameUtils
 {
-	/** 문자열 관련 기능. */
-	std::string PrintF(const char* format, ...);
-	std::wstring PrintF(const wchar_t* format, ...);
-	std::vector<std::string> Split(const std::string& text, const std::string& delimiter);
-	std::vector<std::wstring> Split(const std::wstring& text, const std::wstring& delimiter);
-	std::wstring Convert(const std::string& text);
-	std::string Convert(const std::wstring& text);
-	std::string ToLower(const std::string& text);
-	std::wstring ToLower(const std::wstring& text);
-	std::string ToUpper(const std::string& text);
-	std::wstring ToUpper(const std::wstring& text);
-	bool ToInt(const std::string& integer, int32_t& outInteger);
-	bool ToInt(const std::wstring& integer, int32_t& outInteger);
-	bool ToFloat(const std::string& floating, float& outFloating);
-	bool ToFloat(const std::wstring& floating, float& outFloating);
 
-	/** 파일 관련 기능. */
-	GameError ReadFile(const std::string& path, std::vector<uint8_t>& outBuffer);
-	GameError ReadFile(const std::wstring& path, std::vector<uint8_t>& outBuffer);
-	GameError WriteFile(const std::string& path, const std::vector<uint8_t>& buffer);
-	GameError WriteFile(const std::wstring& path, const std::vector<uint8_t>& buffer);
-	GameError MakeDirectory(const std::string& path);
-	GameError MakeDirectory(const std::wstring& path);
-	bool IsValidPath(const std::string& path);
-	bool IsValidPath(const std::wstring& path);
-	std::string GetBasePath(const std::string& path);
-	std::wstring GetBasePath(const std::wstring& path);
-	std::string RemoveBasePath(const std::string& path);
-	std::wstring RemoveBasePath(const std::wstring& path); 
-	std::string GetFileExtension(const std::string& path);
-	std::wstring GetFileExtension(const std::wstring& path);
+/** 문자열 관련 기능. */
+std::string PrintF(const char* format, ...);
+std::wstring PrintF(const wchar_t* format, ...);
+std::vector<std::string> Split(const std::string& text, const std::string& delimiter);
+std::vector<std::wstring> Split(const std::wstring& text, const std::wstring& delimiter);
+std::wstring Convert(const std::string& text);
+std::string Convert(const std::wstring& text);
+std::string ToLower(const std::string& text);
+std::wstring ToLower(const std::wstring& text);
+std::string ToUpper(const std::string& text);
+std::wstring ToUpper(const std::wstring& text);
+bool ToInt(const std::string& integer, int32_t& outInteger);
+bool ToInt(const std::wstring& integer, int32_t& outInteger);
+bool ToFloat(const std::string& floating, float& outFloating);
+bool ToFloat(const std::wstring& floating, float& outFloating);
+
+/** 파일 관련 기능. */
+GameError ReadFile(const std::string& path, std::vector<uint8_t>& outBuffer);
+GameError ReadFile(const std::wstring& path, std::vector<uint8_t>& outBuffer);
+GameError WriteFile(const std::string& path, const std::vector<uint8_t>& buffer);
+GameError WriteFile(const std::wstring& path, const std::vector<uint8_t>& buffer);
+GameError MakeDirectory(const std::string& path);
+GameError MakeDirectory(const std::wstring& path);
+bool IsValidPath(const std::string& path);
+bool IsValidPath(const std::wstring& path);
+std::string GetBasePath(const std::string& path);
+std::wstring GetBasePath(const std::wstring& path);
+std::string RemoveBasePath(const std::string& path);
+std::wstring RemoveBasePath(const std::wstring& path); 
+std::string GetFileExtension(const std::string& path);
+std::wstring GetFileExtension(const std::wstring& path);
+
 }
