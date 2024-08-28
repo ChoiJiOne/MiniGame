ba4a7546abadc2acd3398f38aa8f80353d617a05
choijione <choijione02@gmail.com>
2024-08-29 07:41:35 +0900

    리소스 인터페이스 추가

---
+++ Core/Inc/IResource.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "GameMacro.h"
+
+/** 이 클래스는 엔진 내의 리소스 인터페이스로, 엔진 내의 리소스(텍스처, 메쉬, 폰트 등등)는 반드시 이 인터페이스를 상속받아야 합니다. */
+class IResource
+{
+public:
+	IResource() = default;
+	virtual ~IResource() 
+	{
+		if (bIsInitialized_)
+		{
+			Release();
+		}
+	}
+
+	DISALLOW_COPY_AND_ASSIGN(IResource);
+
+	virtual void Release() = 0;
+	bool IsInitialized() const { return bIsInitialized_; }
+
+protected:
+	bool bIsInitialized_ = false;
+};
