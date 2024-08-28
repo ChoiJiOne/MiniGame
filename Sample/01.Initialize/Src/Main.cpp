5a08930fffe001eeaa9b1d62c87c2e00f7826fb6
choijione <choijione02@gmail.com>
2024-08-28 13:45:16 +0900

    엔진 초기화/해제 테스트용 코드 추가

--- Sample/01.Initialize/Src/Main.cpp
+++ Sample/01.Initialize/Src/Main.cpp
@@ -5,10 +5,16 @@
 #include <crtdbg.h>
 #endif
 
+#include "GameMaker.h"
+
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
+
+	GameMaker::Startup("01.Initialize", 100, 100, 800, 600);
+	Sleep(1000);
+	GameMaker::Shutdown();
 	return 0;
 }
