7f52bbc8c16a78465c3a6bd79d2a10f4b703f37b
choijione <choijione02@gmail.com>
2024-08-28 16:30:20 +0900

    OpenGL의 에러를 정의하는 에러 코드 및 객체 추가

--- Core/Inc/GameError.h
+++ Core/Inc/GameError.h
@@ -12,7 +12,8 @@ enum class ErrorCode : int32_t
 	FAILED_SDL          = 0x04, /** SDL2 라이브러리 에러 */
 	FAILED_WINDOWS      = 0x05, /** Windows API 에러 */
 	FAILED_IMGUI        = 0x06, /** ImGui 에러 */
-	BUFFER_OUT_OF_RANGE = 0x07, /** 배열의 범위를 벗어난 에러 */
+	FAILED_OPENGL       = 0x07, /** OpenGL 에러 */
+	BUFFER_OUT_OF_RANGE = 0x08, /** 배열의 범위를 벗어난 에러 */
 };
 
 class GameError
@@ -80,6 +81,36 @@ private:
 		uint32_t size = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), buffer, MAX_BUFFER_SIZE, nullptr);
 
 		return std::string(buffer, size);
+	}
+};
+#endif
+
+#if defined(__glad_h_) /** OpenGL이 정의되었을 때. */
+class GLError : public GameError
+{
+public:
+	GLError(GLenum errorCode) : GameError(ErrorCode::FAILED_OPENGL, GetErrorMessage(errorCode)) {}
+	virtual ~GLError() {}
+
+private:
+	static std::string GetErrorMessage(const GLenum errorCode)
+	{
+		static std::map<GLenum, std::string> errorMessages =
+		{
+			{ GL_NO_ERROR,                      "No error has been recorded. (GL_NO_ERROR)"                                                                              },
+			{ GL_INVALID_ENUM,                  "An unacceptable value is specified for an enumerated argument. (GL_INVALID_ENUM)"                                       },
+			{ GL_INVALID_VALUE,                 "A numeric argument is out of range. (GL_INVALID_VALUE)"                                                                 },
+			{ GL_INVALID_OPERATION,             "The specified operation is not allowed in the current state. (GL_INVALID_OPERATION)"                                    },
+			{ GL_INVALID_FRAMEBUFFER_OPERATION, "The framebuffer object is not complete. (GL_INVALID_FRAMEBUFFER_OPERATION)"                                             },
+			{ GL_OUT_OF_MEMORY,                 "There is not enough memory left to execute the command. (GL_OUT_OF_MEMORY)"                                             },
+			{ GL_STACK_UNDERFLOW,               "An attempt has been made to perform an operation that would cause an internal stack to underflow. (GL_STACK_UNDERFLOW)" },
+			{ GL_STACK_OVERFLOW,                "An attempt has been made to perform an operation that would cause an internal stack to overflow. (GL_STACK_OVERFLOW)"   },
+			{ GL_CONTEXT_LOST,                  "This occurs when the OpenGL context becomes lost due to a graphics card reset. (GL_CONTEXT_LOST)"                       },
+			{ GL_NONE,                          "Undefined error message."                                                                                               },
+		};
+
+		auto it = errorMessages.find(errorCode);
+		return (it != errorMessages.end()) ? it->second : errorMessages.at(GL_NONE);
 	}
 };
 #endif
