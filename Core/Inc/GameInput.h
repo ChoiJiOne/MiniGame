520646021305db76d0d508355d4620d49792847d
choijione <choijione02@gmail.com>
2024-08-28 11:59:55 +0900

    입력 상태를 나타내는 열거형 추가

---
+++ Core/Inc/GameInput.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <cstdint>
+
+namespace GameInput
+{
+/**
+ * --------------------------------------
+ * | 이전 프레임 | 현재 프레임 | 입력 상태 |
+ * --------------------------------------
+ * |     0      |     0      | NONE     |
+ * |     0      |     1      | PRESSED  |
+ * |     1      |     0      | RELEASED |
+ * |     1      |     1      | HELD     |
+ * --------------------------------------
+ */
+enum class PressState : int32_t
+{
+	NONE     = 0x00,
+	PRESSED  = 0x01,
+	RELEASED = 0x02,
+	HELD     = 0x03
+};
+
+
+
+
+
+}
