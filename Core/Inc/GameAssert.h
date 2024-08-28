a57d1225db306a2ea20f1d1ac838959d87ddd3ee
choijione <choijione02@gmail.com>
2024-08-29 08:57:25 +0900

    OpenGL 어선셜 매크로에 가변 인자 기반의 메시지를 출력할 수 있는 기능 추가

--- Core/Inc/GameAssert.h
+++ Core/Inc/GameAssert.h
@@ -11,6 +11,7 @@
 	{\
 		GameLogger::DebugPrintF("\nAssertion check point failed!\nFile : %s\nLine : %d\nExpression : %s\nMessage : ", __FILE__, __LINE__, #EXP);\
 		GameLogger::DebugPrintF(__VA_ARGS__);\
+		GameLogger::DebugPrintF("\n");\
 		__debugbreak();\
 	}\
 }
@@ -36,8 +37,23 @@
 
 #if defined(__glad_h_)
 #if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(RELWITHDEBINFO_MODE)
+
 #ifndef GL_ASSERT
-#define GL_ASSERT(EXP)\
+#define GL_ASSERT(EXP, ...)\
+EXP;\
+{\
+	GLenum _errorCode = glGetError();\
+	if (_errorCode != GL_NO_ERROR)\
+	{\
+		GameLogger::DebugPrintF("\nOpenGL Assertion check point failed!\nFile : %s\nLine : %d\nExpression : %s\GL : %s\nMessage : ", __FILE__, __LINE__, #EXP, GLError::GetErrorMessage(_errorCode));\
+		GameLogger::DebugPrintF(__VA_ARGS__);\
+		GameLogger::DebugPrintF("\n");\		
+		__debugbreak();\
+	}\
+}
+#endif
+#ifndef GL_CHECK
+#define GL_CHECK(EXP)\
 EXP;\
 {\
 	GLenum _errorCode = glGetError();\
@@ -50,7 +66,10 @@ EXP;\
 #endif
 #else
 #ifndef GL_ASSERT
-#define GL_ASSERT(EXP) ((void)(EXP))
+#define GL_ASSERT(EXP, ...) ((void)(EXP))
+#endif
+#ifndef GL_CHECK
+#define GL_CHECK(EXP) ((void)(EXP))
 #endif
 #endif
 #endif
