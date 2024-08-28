005e52fad5035dac7198e072223bd83eaa798508
choijione <choijione02@gmail.com>
2024-08-28 17:44:42 +0900

    GameRenderer에 기존의 RenderManager에서 일부 기능을 제외한 나머지 설정 부분 추가

---
+++ Core/Src/GameRenderer.cpp
@@ -0,0 +1,108 @@
+#include <SDL2/SDL.h>
+#include <glad/glad.h>
+#include <imgui.h>
+#include <imgui_impl_opengl3.h>
+
+#include "GameAssert.h"
+#include "GameMaker.h"
+#include "GameRenderer.h"
+
+extern SDL_Window* window_;
+
+void GameRenderer::BeginFrame(float red, float green, float blue, float alpha, float depth, uint8_t stencil)
+{
+	SetWindowViewport();
+
+	glClearColor(red, green, blue, alpha);
+	glClearDepth(depth);
+	glClearStencil(stencil);
+	
+	GL_ASSERT(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT));
+}
+
+void GameRenderer::EndFrame()
+{
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+	SDL_GL_SwapWindow(window_);
+}
+
+void GameRenderer::SetViewport(int32_t x, int32_t y, int32_t width, int32_t height)
+{
+	glViewport(x, y, width, height);
+}
+
+void GameRenderer::SetWindowViewport()
+{
+	int32_t w = 0;
+	int32_t h = 0;
+	GameMaker::GetScreenSize<int32_t>(w, h);
+	SetViewport(0, 0, w, h);
+}
+
+void GameRenderer::SetVsyncMode(bool bIsEnable)
+{
+	GL_ASSERT(SDL_GL_SetSwapInterval(static_cast<int32_t>(bIsEnable)));
+}
+
+void GameRenderer::SetDepthMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_ASSERT(glEnable(GL_DEPTH_TEST));
+	}
+	else
+	{
+		GL_ASSERT(glDisable(GL_DEPTH_TEST));
+	}
+}
+
+void GameRenderer::SetStencilMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_ASSERT(glEnable(GL_STENCIL_TEST));
+	}
+	else
+	{
+		GL_ASSERT(glDisable(GL_STENCIL_TEST));
+	}
+}
+
+void GameRenderer::SetAlphaBlendMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_ASSERT(glEnable(GL_BLEND));
+		GL_ASSERT(glBlendFuncSeparate(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA, GL_ONE, GL_ZERO));
+	}
+	else
+	{
+		GL_ASSERT(glDisable(GL_BLEND));
+	}
+}
+
+void GameRenderer::SetMultisampleMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_ASSERT(glEnable(GL_MULTISAMPLE));
+	}
+	else
+	{
+		GL_ASSERT(glDisable(GL_MULTISAMPLE));
+	}
+}
+
+void GameRenderer::SetCullFaceMode(bool bIsEnable)
+{
+	if (bIsEnable)
+	{
+		GL_ASSERT(glEnable(GL_CULL_FACE));
+	}
+	else
+	{
+		GL_ASSERT(glDisable(GL_CULL_FACE));
+	}
+}
