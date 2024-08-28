06586a1ec139f6383f35d3806f38b5fd1d4d07fb
choijione <choijione02@gmail.com>
2024-08-28 17:35:23 +0900

    OpenGL API 호출 여부를 검사하기 위한 매크로 추가

--- Core/Inc/GameAssert.h
+++ Core/Inc/GameAssert.h
@@ -19,3 +19,24 @@
 #define GAME_ASSERT(EXP, MESSAGE) ((void)(EXP))
 #endif
 #endif
+
+#if defined(__glad_h_)
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(RELWITHDEBINFO_MODE)
+#ifndef GL_ASSERT
+#define GL_ASSERT(EXP)\
+EXP;\
+{\
+	GLenum _errorCode = glGetError();\
+	if (_errorCode != GL_NO_ERROR)\
+	{\
+		GameLogger::DebugPrintF("\nOpenGL Assertion check point failed!\nFile : %s\nLine : %d\nExpression : %s\nMessage : %s\n", __FILE__, __LINE__, #EXP, GLError::GetErrorMessage(_errorCode));\
+		__debugbreak();\
+	}\
+}
+#endif
+#else
+#ifndef GL_ASSERT
+#define GL_ASSERT(EXP) ((void)(EXP))
+#endif
+#endif
+#endif
