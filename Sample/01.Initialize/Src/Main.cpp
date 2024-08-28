e5f9dd17c14efab7c602b415048e4d2db6ff1d3a
choijione <choijione02@gmail.com>
2024-08-28 17:47:23 +0900

    백버퍼를 흰색으로 초기화를 수행하는 샘플 추가

--- Sample/01.Initialize/Src/Main.cpp
+++ Sample/01.Initialize/Src/Main.cpp
@@ -5,7 +5,9 @@
 #include <crtdbg.h>
 #endif
 
+#include "GameAssert.h"
 #include "GameMaker.h"
+#include "GameRenderer.h"
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
 {
@@ -13,8 +15,17 @@ int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstan
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
 #endif
 
-	GameMaker::Startup("01.Initialize", 100, 100, 800, 600);
-	Sleep(1000);
+	GameError error = GameMaker::Startup("01.Initialize", 100, 100, 800, 600);
+	GAME_ASSERT(error.GetCode() == ErrorCode::OK, error.GetMessagePtr());
+
+	GameMaker::RunLoop(
+		[](float deltaSeconds) 
+		{
+			GameRenderer::BeginFrame(1.0f, 1.0f, 1.0f, 1.0f);
+			GameRenderer::EndFrame();
+		}
+	);
+
 	GameMaker::Shutdown();
 	return 0;
 }
