8895a8c99dea25acc32143d49bb070d91ad457c6
choijione <choijione02@gmail.com>
2024-08-30 10:50:34 +0900

    크래시 덤프 파일을 생성하는 02.Crash 샘플 추가

---
+++ Sample/02.Crash/Src/Main.cpp
@@ -0,0 +1,63 @@
+#include <cstdint>
+#include <Windows.h>
+
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+#include <crtdbg.h>
+#endif
+
+#include "Assertion.h"
+#include "IApp.h"
+#include "CrashManager.h"
+#include "GameUtils.h"
+
+class DemoApp : public IApp
+{
+public:
+	DemoApp() : IApp("02.Crash", 100, 100, 800, 600, false, false) {}
+	virtual ~DemoApp() {}
+
+	DISALLOW_COPY_AND_ASSIGN(DemoApp);
+
+	virtual void Startup() override
+	{
+		CrashManager::Get().SetCrashCallbackEvent(
+			[&]() 
+			{
+				::MessageBoxW(NULL, CrashManager::Get().GetCrashDumpFileNamePtr(), L"DETECT CRASH!", MB_OK);
+			}
+		);
+	}
+
+	virtual void Shutdown() override
+	{
+	}
+
+	virtual void Run() override
+	{
+		RunLoop(
+			[&](float deltaSeconds)
+			{
+				BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+				int* a = nullptr;
+				*a = 10; /** 고의로 크래시 유발! */
+				EndFrame();
+			}
+		);
+	}
+
+private:
+
+};
+
+int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
+{
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	std::unique_ptr<IApp> app = std::make_unique<DemoApp>();
+	app->Startup();
+	app->Run();
+	app->Shutdown();
+	return 0;
+}
