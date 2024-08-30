1ee2328b622c555480d62db99dd73b9ce414ba2c
choijione <choijione02@gmail.com>
2024-08-30 11:41:25 +0900

    기존의 EntityManager에서 Startup/Shutdown 및 상속 구조 삭제 후 추가
    - 엔진의 변경 사항에 맞게 수정

---
+++ Core/Inc/EntityManager.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <array>
+#include <memory>
+
+#include "IEntity.h"
+
+/** 엔티티 매니저는 싱글턴입니다. */
+class EntityManager
+{
+public:
+	DISALLOW_COPY_AND_ASSIGN(EntityManager);
+
+	static EntityManager& Get();
+
+	template <typename TEntity, typename... Args>
+	TEntity* Create(Args&&... args)
+	{
+		if (!(0 <= cacheSize_ && cacheSize_ < MAX_ENTITY_SIZE))
+		{
+			return nullptr;
+		}
+
+		int32_t entityID = -1;
+		for (uint32_t index = 0; index < cacheSize_; ++index)
+		{
+			if (!entities_[index] && !usage_[index])
+			{
+				entityID = static_cast<int32_t>(index);
+				break;
+			}
+		}
+
+		if (entityID == -1)
+		{
+			entityID = cacheSize_++;
+		}
+
+		usage_[entityID] = true;
+		entities_[entityID] = std::make_unique<TEntity>(args...);
+
+		return reinterpret_cast<TEntity*>(entities_[entityID].get());
+	}
+
+	void Destroy(const IEntity* entity);
+
+private:
+	friend class IApp;
+
+	EntityManager() = default;
+	virtual ~EntityManager() {}
+
+	void Cleanup();
+
+private:
+	static const uint32_t MAX_ENTITY_SIZE = 200;
+
+	uint32_t cacheSize_ = 0;
+	std::array<std::unique_ptr<IEntity>, MAX_ENTITY_SIZE> entities_;
+	std::array<bool, MAX_ENTITY_SIZE> usage_;
+};
---
+++ Core/Src/EntityManager.cpp
@@ -0,0 +1,52 @@
+#include "Assertion.h"
+#include "EntityManager.h"
+
+EntityManager& EntityManager::Get()
+{
+	static EntityManager instance;
+	return instance;
+}
+
+void EntityManager::Destroy(const IEntity* entity)
+{
+	int32_t entityID = -1;
+	for (uint32_t index = 0; index < cacheSize_; ++index)
+	{
+		IEntity* cache = entities_[index].get();
+		if (entity == cache)
+		{
+			entityID = static_cast<int32_t>(index);
+			break;
+		}
+	}
+
+	if (entityID != -1 && entities_[entityID])
+	{
+		if (entities_[entityID]->IsInitialized())
+		{
+			entities_[entityID]->Release();
+		}
+
+		entities_[entityID].reset();
+		usage_[entityID] = false;
+	}
+}
+
+void EntityManager::Cleanup()
+{
+	for (std::size_t index = 0; index < cacheSize_; ++index)
+	{
+		if (entities_[index])
+		{
+			if (entities_[index]->IsInitialized())
+			{
+				entities_[index]->Release();
+			}
+
+			entities_[index].reset();
+			usage_[index] = false;
+		}
+	}
+
+	cacheSize_ = 0;
+}
