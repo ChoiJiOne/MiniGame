916ad56ab9348cb5cf29075ee32c78b420f10d31
choijione <choijione02@gmail.com>
2024-08-30 11:33:47 +0900

    기존의 ResourceManager에서 Startup/Shutdown 및 상속 구조 삭제 후 추가
    - 엔진의 변경 사항에 맞게 수정

--- Core/Inc/ResourceManager.h
+++ Core/Inc/ResourceManager.h
@@ -5,6 +5,7 @@
 
 #include "IResource.h"
 
+/** 리소스 매니저는 싱글턴 클래스입니다. */
 class ResourceManager
 {
 public:
@@ -12,10 +13,49 @@ public:
 
 	static ResourceManager& Get();
 
+	template <typename TResource, typename... Args>
+	TResource* Create(Args&&... args)
+	{
+		if (!(0 <= cacheSize_ && cacheSize_ < MAX_RESOURCE_SIZE))
+		{
+			return nullptr;
+		}
+
+		int32_t resourceID = -1;
+		for (uint32_t index = 0; index < cacheSize_; ++index)
+		{
+			if (!resources_[index] && !usage_[index])
+			{
+				resourceID = static_cast<int32_t>(index);
+				break;
+			}
+		}
+
+		if (resourceID == -1)
+		{
+			resourceID = cacheSize_++;
+		}
+
+		usage_[resourceID] = true;
+		resources_[resourceID] = std::make_unique<TResource>(args...);
+
+		return reinterpret_cast<TResource*>(resources_[resourceID].get());
+	}
+
+	void Destroy(const IResource* resource);
+
 private:
+	friend class IApp;
+
 	ResourceManager() = default;
 	virtual ~ResourceManager() {}
+	
+	void Cleanup(); /** 이 메서드는 IApp에서만 접근해야 합니다. */
 
 private:
+	static const uint32_t MAX_RESOURCE_SIZE = 200;
 
+	uint32_t cacheSize_ = 0;
+	std::array<std::unique_ptr<IResource>, MAX_RESOURCE_SIZE> resources_;
+	std::array<bool, MAX_RESOURCE_SIZE> usage_;
 };
--- Core/Src/ResourceManager.cpp
+++ Core/Src/ResourceManager.cpp
@@ -1,3 +1,52 @@
 #include "Assertion.h"
 #include "ResourceManager.h"
 
+ResourceManager& ResourceManager::Get()
+{
+	static ResourceManager instance;
+	return instance;
+}
+
+void ResourceManager::Destroy(const IResource* resource)
+{
+	int32_t resourceID = -1;
+	for (uint32_t index = 0; index < cacheSize_; ++index)
+	{
+		IResource* cacheResource = resources_[index].get();
+		if (resource == cacheResource)
+		{
+			resourceID = static_cast<int32_t>(index);
+			break;
+		}
+	}
+
+	if (resourceID != -1 && resources_[resourceID])
+	{
+		if (resources_[resourceID]->IsInitialized())
+		{
+			resources_[resourceID]->Release();
+		}
+
+		resources_[resourceID].reset();
+		usage_[resourceID] = false;
+	}
+}
+
+void ResourceManager::Cleanup()
+{
+	for (std::size_t index = 0; index < cacheSize_; ++index)
+	{
+		if (resources_[index])
+		{
+			if (resources_[index]->IsInitialized())
+			{
+				resources_[index]->Release();
+			}
+
+			resources_[index].reset();
+			usage_[index] = false;
+		}
+	}
+
+	cacheSize_ = 0;
+}
