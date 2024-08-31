d5187b5b0ef54c2139a559d5f52daa779ef42fbb
choijione <choijione02@gmail.com>
2024-08-31 14:31:01 +0900

    OpenGL의 S3TC 관련 매크로 선언 추가
    - https://registry.khronos.org/OpenGL/extensions/EXT/EXT_texture_compression_s3tc.txt

--- ThirdParty/glad/include/glad/glad.h
+++ ThirdParty/glad/include/glad/glad.h
@@ -3691,6 +3691,12 @@ GLAPI PFNGLPOLYGONOFFSETCLAMPPROC glad_glPolygonOffsetClamp;
 #define glPolygonOffsetClamp glad_glPolygonOffsetClamp
 #endif
 
+/** S3TC 확장 기능 (GL_EXT_texture_compression_s3tc) 선언 */
+#define GL_COMPRESSED_RGB_S3TC_DXT1_EXT 0x83F0
+#define GL_COMPRESSED_RGBA_S3TC_DXT1_EXT 0x83F1
+#define GL_COMPRESSED_RGBA_S3TC_DXT3_EXT 0x83F2
+#define GL_COMPRESSED_RGBA_S3TC_DXT5_EXT 0x83F3
+
 #ifdef __cplusplus
 }
 #endif
