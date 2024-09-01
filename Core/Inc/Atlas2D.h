4fdef58454c13cfe79244ec92b3d529b571053b6
choijione <choijione02@gmail.com>
2024-09-01 16:22:30 +0900

    Json 파일을 기반으로 텍스처 아틀라스 리소스를 구성하는 기능 추가

---
+++ Core/Inc/Atlas2D.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <map>
+
+#include "GameMath.h"
+#include "Texture2D.h"
+
+class Atlas2D : public Texture2D
+{
+public:
+	struct Block /** 텍스처 내의 블럭입니다. */
+	{
+		GameMath::Vec2i pos;
+		GameMath::Vec2i size;
+	};
+
+public:
+	Atlas2D(const std::string& imagePath, const std::string& jsonPath, const Filter& filter);
+	virtual ~Atlas2D();
+
+	DISALLOW_COPY_AND_ASSIGN(Atlas2D);
+
+	virtual void Release() override;
+
+	const Block& GetByName(const std::string& name) const;
+
+private:
+	std::map<std::string, Block> blocks_;
+};
---
+++ Core/Src/Atlas2D.cpp
@@ -0,0 +1,56 @@
+#include <json/json.h>
+
+#include "Assertion.h"
+#include "Atlas2D.h"
+#include "GameUtils.h"
+#include "IApp.h"
+
+Atlas2D::Atlas2D(const std::string& imagePath, const std::string& jsonPath, const Filter& filter)
+	: Texture2D(imagePath, filter)
+{
+	std::vector<uint8_t> buffer = GameUtils::ReadFile(jsonPath);
+	std::string jsonString(buffer.begin(), buffer.end());
+
+	Json::Value root;
+	Json::Reader reader;
+	ASSERT(reader.parse(jsonString, root), "Failed to parse '%s' file.", jsonPath.c_str());
+
+	for (const auto& key : root.getMemberNames())
+	{
+		const Json::Value& element = root[key];
+
+		CHECK(element["x"].isInt() && !element["x"].isNull());
+		CHECK(element["y"].isInt() && !element["y"].isNull());
+		CHECK(element["w"].isInt() && !element["w"].isNull());
+		CHECK(element["h"].isInt() && !element["h"].isNull());
+		
+		Block block;
+		block.pos.x = element["x"].asInt();
+		block.pos.y = element["y"].asInt();
+		block.size.x = element["w"].asInt();
+		block.size.y = element["h"].asInt();
+
+		blocks_.insert({ key.c_str(), block });
+	}
+}
+
+Atlas2D::~Atlas2D()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void Atlas2D::Release()
+{
+	Texture2D::Release();
+}
+
+const Atlas2D::Block& Atlas2D::GetByName(const std::string& name) const
+{
+	auto it = blocks_.find(name);
+	CHECK(it != blocks_.end());
+
+	return it->second;
+}
