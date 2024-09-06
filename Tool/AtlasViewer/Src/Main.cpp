928601847c53f071f4397a6cff85f5ed22cd067e
choijione <choijione02@gmail.com>
2024-09-06 11:51:58 +0900

    엔진에서 지원하는 텍스처 아틀라스를 확인하기 위한 툴 프로젝트 추가

---
+++ Tool/AtlasViewer/Src/Main.cpp
@@ -0,0 +1,50 @@
+#include <cstdint>
+#include <Windows.h>
+
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+#include <crtdbg.h>
+#endif
+
+#include "Assertion.h"
+#include "IApp.h"
+
+class AtlasViewer : public IApp
+{
+public:
+	AtlasViewer() : IApp("AtlasViewer", 100, 100, 800, 600, false, false) {}
+	virtual ~AtlasViewer() {}
+
+	DISALLOW_COPY_AND_ASSIGN(AtlasViewer);
+
+	virtual void Startup() override
+	{
+	}
+
+	virtual void Shutdown() override
+	{
+	}
+
+	virtual void Run() override
+	{
+		RunLoop(
+			[&](float deltaSeconds)
+			{
+				BeginFrame(1.0f, 0.0f, 0.0f, 1.0f);
+				EndFrame();
+			}
+		);
+	}
+};
+
+int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
+{
+#if defined(DEBUG_MODE) || defined(RELEASE_MODE) || defined(DEVELOPMENT_MODE)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	std::unique_ptr<IApp> app = std::make_unique<AtlasViewer>();
+	app->Startup();
+	app->Run();
+	app->Shutdown();
+	return 0;
+}
