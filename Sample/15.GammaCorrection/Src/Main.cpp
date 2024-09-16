a6537219fff166bcf1f303900c48cccd39da8420
choijione <choijione02@gmail.com>
2024-09-16 11:19:14 +0900

    감마 보정 적용을 확인하기 위한 그라데이션 사각형들을 그리는 기능 추가

--- Sample/15.GammaCorrection/Src/Main.cpp
+++ Sample/15.GammaCorrection/Src/Main.cpp
@@ -10,6 +10,15 @@
 
 #include "Assertion.h"
 #include "IApp.h"
+#include "ResourceManager.h"
+#include "Shader.h"
+#include "VertexBuffer.h"
+
+struct Vertex
+{
+	GameMath::Vec3f position;
+	GameMath::Vec4f color;
+};
 
 class DemoApp : public IApp
 {
@@ -21,10 +30,86 @@ public:
 
 	virtual void Startup() override
 	{
+		shader_ = ResourceManager::Get().Create<Shader>(
+			"GameMaker\\Sample\\15.GammaCorrection\\Res\\Shader.vert", 
+			"GameMaker\\Sample\\15.GammaCorrection\\Res\\Shader.frag"
+		);
+
+		{ // Object 1.
+			std::array<Vertex, 4> vertices =
+			{
+				Vertex{GameMath::Vec3f(-0.9f, +0.2f, 0.0f), GameMath::Vec4f(0.0f, 0.0f, 0.0f, 1.0f) },
+				Vertex{GameMath::Vec3f(+0.9f, +0.2f, 0.0f), GameMath::Vec4f(1.0f, 1.0f, 1.0f, 1.0f) },
+				Vertex{GameMath::Vec3f(+0.9f, +0.4f, 0.0f), GameMath::Vec4f(1.0f, 1.0f, 1.0f, 1.0f) },
+				Vertex{GameMath::Vec3f(-0.9f, +0.4f, 0.0f), GameMath::Vec4f(0.0f, 0.0f, 0.0f, 1.0f) },
+			};
+
+			uint32_t stride = static_cast<uint32_t>(sizeof(Vertex));
+			uint32_t byteSize = static_cast<uint32_t>(vertices.size()) * stride;
+			VertexBuffer* vertexBuffer = ResourceManager::Get().Create<VertexBuffer>(vertices.data(), byteSize, VertexBuffer::Usage::STATIC);
+
+			uint32_t vao;
+			GL_CHECK(glGenVertexArrays(1, &vao));
+			GL_CHECK(glBindVertexArray(vao));
+			{
+				vertexBuffer->Bind();
+
+				GL_CHECK(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)(offsetof(Vertex, position))));
+				GL_CHECK(glEnableVertexAttribArray(0));
+
+				GL_CHECK(glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, stride, (void*)(offsetof(Vertex, color))));
+				GL_CHECK(glEnableVertexAttribArray(1));
+
+				vertexBuffer->Unbind();
+			}
+			GL_CHECK(glBindVertexArray(0));
+
+			objects_.push_back(std::pair{ vao, vertexBuffer });
+		}
+
+		{ // Object 2.
+			std::array<Vertex, 4> vertices =
+			{
+				Vertex{GameMath::Vec3f(-0.9f, -0.4f, 0.0f), GameMath::Vec4f(0.0f, 0.0f, 0.0f, 1.0f) },
+				Vertex{GameMath::Vec3f(+0.9f, -0.4f, 0.0f), GameMath::Vec4f(1.0f, 1.0f, 1.0f, 1.0f) },
+				Vertex{GameMath::Vec3f(+0.9f, -0.2f, 0.0f), GameMath::Vec4f(1.0f, 1.0f, 1.0f, 1.0f) },
+				Vertex{GameMath::Vec3f(-0.9f, -0.2f, 0.0f), GameMath::Vec4f(0.0f, 0.0f, 0.0f, 1.0f) },
+			};
+
+			uint32_t stride = static_cast<uint32_t>(sizeof(Vertex));
+			uint32_t byteSize = static_cast<uint32_t>(vertices.size()) * stride;
+			VertexBuffer* vertexBuffer = ResourceManager::Get().Create<VertexBuffer>(vertices.data(), byteSize, VertexBuffer::Usage::STATIC);
+
+			uint32_t vao;
+			GL_CHECK(glGenVertexArrays(1, &vao));
+			GL_CHECK(glBindVertexArray(vao));
+			{
+				vertexBuffer->Bind();
+
+				GL_CHECK(glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, (void*)(offsetof(Vertex, position))));
+				GL_CHECK(glEnableVertexAttribArray(0));
+
+				GL_CHECK(glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, stride, (void*)(offsetof(Vertex, color))));
+				GL_CHECK(glEnableVertexAttribArray(1));
+
+				vertexBuffer->Unbind();
+			}
+			GL_CHECK(glBindVertexArray(0));
+
+			objects_.push_back(std::pair{ vao, vertexBuffer });
+		}
 	}
 
 	virtual void Shutdown() override
 	{
+		for (auto& object : objects_)
+		{
+			GL_CHECK(glDeleteVertexArrays(1, &object.first));
+			ResourceManager::Get().Destroy(object.second);
+		}
+
+		ResourceManager::Get().Destroy(shader_);
+		shader_ = nullptr;
 	}
 
 	virtual void Run() override
@@ -33,10 +118,25 @@ public:
 			[&](float deltaSeconds)
 			{
 				BeginFrame(0.0f, 0.0f, 0.0f, 1.0f);
+				shader_->Bind();
+				{
+					for (auto& object : objects_)
+					{
+						GL_CHECK(glBindVertexArray(object.first));
+						GL_CHECK(glDrawArrays(static_cast<GLenum>(DrawMode::TRIANGLE_FAN), 0, 4));
+						GL_CHECK(glBindVertexArray(0));
+					}
+				}
+				shader_->Unbind();
 				EndFrame();
 			}
 		);
 	}
+
+private:
+	Shader* shader_ = nullptr;
+
+	std::vector<std::pair<uint32_t, VertexBuffer*>> objects_;
 };
 
 int32_t WINAPI wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR pCmdLine, _In_ int32_t nCmdShow)
