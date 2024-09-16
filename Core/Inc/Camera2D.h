d5b2fa018416f584c6339c416041b6ef002fe18b
choijione <choijione02@gmail.com>
2024-09-16 13:18:14 +0900

    2D 카메라의 가로/세로 크기를 2차원 벡터로 수정

--- Core/Inc/Camera2D.h
+++ Core/Inc/Camera2D.h
@@ -15,26 +15,19 @@ public:
 	virtual void Release() = 0;
 
 	const GameMath::Vec2f& GetCenter() const { return center_; }
-	const float& GetWidth() const { return width_; }
-	const float& GetHeight() const { return height_; }
+	const GameMath::Vec2f& GetSize() const { return size_; }
+	const float& GetWidth() const { return size_.x; }
+	const float& GetHeight() const { return size_.y; }
 	const GameMath::Mat4x4& GetOrtho() const { return ortho_; }
 
 protected:
-	GameMath::Mat4x4 CalculateOrtho(const GameMath::Vec2f& center, float width, float height, float zNear = -1.0f, float zFar = 1.0f)
+	GameMath::Mat4x4 CalculateOrtho(const GameMath::Vec2f& center, const GameMath::Vec2f& size, float zNear = -1.0f, float zFar = 1.0f)
 	{
-		return GameMath::Mat4x4::Ortho(
-			-width * 0.5f + center.x, 
-			+width * 0.5f + center.x,
-			-height * 0.5f + center.y, 
-			+height * 0.5f + center.y,
-			zNear, 
-			zFar
-		);
+		return GameMath::Mat4x4::Ortho(-size.x * 0.5f + center.x, +size.x * 0.5f + center.x, -size.y * 0.5f + center.y, +size.y * 0.5f + center.y, zNear, zFar);
 	}
 
 protected:
 	GameMath::Vec2f center_;
-	float width_ = 0.0f;
-	float height_ = 0.0f;
+	GameMath::Vec2f size_;
 	GameMath::Mat4x4 ortho_;
 };
