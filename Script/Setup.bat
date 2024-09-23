7f92a99cef59700d456b914114a2f364b433b345
choijione <choijione02@gmail.com>
2024-09-24 08:19:23 +0900

    셋업 배치 스크립트에서 프로젝트의 이름을 검사하는 부분 복원

--- Script/Setup.bat
+++ Script/Setup.bat
@@ -15,6 +15,23 @@ IF %ERRORLEVEL% EQU 0 (
     GOTO:EOF
 )
 
+SET PROJECT_NAME=%1
+if "%PROJECT_NAME%" == "" (
+    ECHO Project name is empty...
+    ECHO ====================
+    ECHO Done Setup Project
+    ECHO ====================
+    GOTO:EOF
+) else if "%PROJECT_NAME%" == "GameMaker" (
+    ECHO Project name "%PROJECT_NAME%" is invalid...
+    ECHO ====================
+    ECHO Done Setup Project
+    ECHO ====================
+    GOTO:EOF
+) else (
+    ECHO Project name is "%PROJECT_NAME%"...
+)
+
 SET CURRENT_PATH=%~dp0
 
 PUSHD "%CURRENT_PATH%"
