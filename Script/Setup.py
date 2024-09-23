5f6fe24b2b3c51657fa3afe347d63460d0a6100b
choijione <choijione02@gmail.com>
2024-09-24 07:26:59 +0900

    템플릿 파일의 위치 변경 사항 적용

--- Script/Setup.py
+++ Script/Setup.py
@@ -14,32 +14,32 @@ if __name__ == "__main__":
     project_name = "Tetris3D"
 
     root_path = os.getcwd()
-    template_path = root_path + "\\GameMaker\\Template\\"
+    path = root_path + "\\GameMaker\\"
     project_path = root_path + "\\" + project_name
 
     modes = [ "Debug", "MinSizeRel", "Release", "RelWithDebInfo", ]
 
     build_scripts = {}
-    build_script_template = read_text_file(template_path + "BuildScript.bat")
+    build_script_template = read_text_file(path + "Script\\BuildSolution_Template.bat")
     for mode in modes:
         script = build_script_template.format(MODE=mode)
         build_scripts[mode] = script
 
     package_scripts = {}
-    package_script_template = read_text_file(template_path + "PackageScript.bat")
+    package_script_template = read_text_file(path + "Script\\Package_Template.bat")
     for mode in modes:
         script = package_script_template.format(MODE=mode)
         package_scripts[mode] = script
 
     project_setting_scripts = {}
-    project_setting_script_template = read_text_file(template_path + "ProjectSettingScript.bat")
+    project_setting_script_template = read_text_file(path + "Script\\GenerateProjectFiles_Template.bat")
     project_setting_scripts["HotReload"] = project_setting_script_template.format(NAME=project_name, OPTION="Off")
     project_setting_scripts["GenerateProjectFiles"] = project_setting_script_template.format(NAME=project_name, OPTION="On")
 
-    solution_script_template = read_text_file(template_path + "SolutionScript.txt")
+    solution_script_template = read_text_file(path + "CMake\\Solution_CMakeLists_Template.txt")
     solution_script = solution_script_template.replace("${SOLUTION_NAME}", project_name, 2)
 
-    project_script_template = read_text_file(template_path + "ProjectScript.txt")
+    project_script_template = read_text_file(path + "CMake\\Project_CMakeLists_Template.txt")
     project_script = project_script_template.replace("{NAME}", project_name, 1)
     
     print(f"\nSuccessed setup {project_name}!")
