9ae2da0af4d6c27e7110faadb2d01d450a649b29
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-09-23 17:12:57 +0900

    각 모드에 맞는 빌드 스크립트를 구성

--- Script/Setup.py
+++ Script/Setup.py
@@ -1,57 +1,28 @@
 import sys
 import os
 
-def is_already_setup()->bool:
-    for setup_target_file in setup_target_files.keys():
-        is_already_exist = os.path.exists(setup_target_file)
-        output_message = f"[CHECK] {setup_target_file} => "
-
-        if is_already_exist:
-            output_message += "Failed."
-            print(output_message)
-            return True
-        
-        output_message += "Ok."
-        print(output_message)
-
-    return False
+def read_text_file(path)->str:
+    with open(path, 'r') as file:
+        content = file.read()
+        return content
 
 def write_text_file(path, text):
     with open(path, 'w') as file:
         file.write(text)
     
 if __name__ == "__main__":
-    project_name = sys.argv[1]
-    option = sys.argv[2]
-    
+    project_name = "Tetris3D"
+
     root_path = os.getcwd()
     template_path = root_path + "\\GameMaker\\Template\\"
     project_path = root_path + "\\" + project_name
 
-    if is_already_setup():
-        print(f"Failed setup {project_name}!")
-        sys.exit()
+    modes = [ "Debug", "MinSizeRel", "Release", "RelWithDebInfo", ]
 
-    template_file_path = f"{root_path}"
-    gitignore_file = setup_target_files[f"{root_path}\\.gitignore"]
-    if option == "--ignore":
-        gitignore_file += f"\n\n{project_name}\n"
-        gitignore_file += "Build_Debug.bat\n"
-        gitignore_file += "Build_MinSizeRel.bat\n"
-        gitignore_file += "Build_RelWithDebInfo.bat\n"
-        gitignore_file += "Build_Release.bat\n"
-        gitignore_file += "CMakeLists.txt\n"
-        gitignore_file += "GenerateProjectFiles.bat\n"
-        gitignore_file += "HotReload.bat\n"
-        gitignore_file += "LICENSE.txt\n"
-        gitignore_file += "Package_Debug.bat\n"
-        gitignore_file += "Package_MinSizeRel.bat\n"
-        gitignore_file += "Package_RelWithDebInfo.bat\n"
-        gitignore_file += "Package_Release.bat"
-        
-        setup_target_files[f"{root_path}\\.gitignore"] = gitignore_file
-
-    for path, text in setup_target_files.items():
-        write_text_file(path, text)
+    build_scripts = {}
+    build_script_template = read_text_file(template_path + "BuildScript.bat")
+    for mode in modes:
+        script = build_script_template.format(MODE=mode)
+        build_scripts[mode] = script
 
     print(f"Successed setup {project_name}!")
