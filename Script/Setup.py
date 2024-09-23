438bea33e9f9ed4d3a10f8ce52b54e537c2baed7
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-09-23 17:38:49 +0900

    프로젝트에 맞는 솔루션 스크립트 생성

--- Script/Setup.py
+++ Script/Setup.py
@@ -35,5 +35,11 @@ if __name__ == "__main__":
     project_script_template = read_text_file(template_path + "ProjectScript.bat")
     project_scripts["HotReload"] = project_script_template.format(NAME=project_name, OPTION="Off")
     project_scripts["GenerateProjectFiles"] = project_script_template.format(NAME=project_name, OPTION="On")
+
+    solution_script_template = read_text_file(template_path + "SolutionScript.txt")
+    solution_script = solution_script_template.replace("${SOLUTION_NAME}", project_name, 2)
+
     
+
+
     print(f"Successed setup {project_name}!")
