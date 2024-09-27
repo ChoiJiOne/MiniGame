8d881a4707e6c640467edd15a789f769410af653
DESKTOP-2500GIQ\Admin <choijione02@gmail.com>
2024-09-27 12:45:56 +0900

    스프라이트 렌더링 옵션을 나타내는 구조체 추가

--- Core/Inc/RenderManager2D.h
+++ Core/Inc/RenderManager2D.h
@@ -26,6 +26,15 @@ class VertexBuffer;
 class RenderManager2D
 {
 public:
+	struct SpriteRenderOptions
+	{
+		Vec3f blend; /** 스프라이트와 혼합할 색상 입니다. */
+		float factor; /** 스프라이트와 혼합할 색상의 비율입니다. factor의 값이 0이라면 색상이 혼합되지 않습니다. */
+		bool bIsFlipH = false; /** 스프라이트를 가로로 뒤집을 지 여부입니다. */
+		bool bIsFlipV = false; /** 스프라이트를 세로로 뒤집을 지 여부입니다. */
+	};
+
+public:
 	DISALLOW_COPY_AND_ASSIGN(RenderManager2D);
 
 	static RenderManager2D& Get();
