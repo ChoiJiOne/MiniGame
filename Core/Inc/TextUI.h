384384d7d34aa6f5259160efd172c1e00fd113a2
choijione <choijione02@gmail.com>
2024-10-15 18:04:48 +0900

    텍스트 UI 추가

---
+++ Core/Inc/TextUI.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Collision2D.h"
+#include "IEntityUI.h"
+
+class TTFont;
+
+class TextUI : public IEntityUI
+{
+public:
+	struct Layout
+	{
+		Vec4f textColor;
+		Vec2f textCenterPos; /** 텍스트의 중심 좌표 */
+		TTFont* font = nullptr;
+		std::wstring text;
+	};
+
+public:
+	TextUI(const Layout& layout);
+	virtual ~TextUI();
+
+	DISALLOW_COPY_AND_ASSIGN(TextUI);
+
+	virtual void Tick(float deltaSeconds) override;
+	virtual void Release() override;
+	virtual Type GetType() const override { return Type::TEXT; }
+
+private:
+	friend class UIManager;
+
+private:
+	Layout layout_;
+	Vec2f textSize_;
+	Vec2f textPos_; /** 렌더링 시 참조할 텍스트의 위치. */
+};
---
+++ Core/Src/TextUI.cpp
@@ -0,0 +1,31 @@
+#include "Assertion.h"
+#include "TextUI.h"
+#include "TTFont.h"
+
+TextUI::TextUI(const Layout& layout)
+	: layout_(layout)
+{
+	layout_.font->MeasureText(layout_.text, textSize_.x, textSize_.y);
+	textPos_ = layout_.textCenterPos + Vec2f(-textSize_.x * 0.5f, +textSize_.y * 0.5f);
+
+	bIsInitialized_ = true;
+}
+
+TextUI::~TextUI()
+{
+	if (bIsInitialized_)
+	{
+		Release();
+	}
+}
+
+void TextUI::Tick(float deltaSeconds)
+{
+}
+
+void TextUI::Release()
+{
+	CHECK(bIsInitialized_);
+
+	bIsInitialized_ = false;
+}
