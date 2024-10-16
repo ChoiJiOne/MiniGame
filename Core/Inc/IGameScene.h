42b4acc39e6943bc3cb8247443163521289fc970
choijione <choijione02@gmail.com>
2024-10-16 17:31:12 +0900

    씬 인터페이스 추가

---
+++ Core/Inc/IGameScene.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Macro.h"
+
+/** 게임 내의 씬 인터페이스입니다. */
+class IGameScene
+{
+public:
+	IGameScene() = default;
+	virtual ~IGameScene() {}
+
+	DISALLOW_COPY_AND_ASSIGN(IGameScene);
+
+	virtual void Tick(float deltaSeconds) = 0;
+	virtual void Render() = 0;
+	
+	virtual bool IsSceneSwitched() = 0;
+	virtual IGameScene* GetSwitchScene() = 0;
+};
